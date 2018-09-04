57964163fa0591fa98b3acac0382067e8020efe9
Makentoshe <mkliverout@gmail.com>
2018-09-04 22:08:06 +0300

    Refactoring Post and add test

--- app/src/main/kotlin/com/makentoshe/booruchan/boors/entity/Post.kt
+++ app/src/main/kotlin/com/makentoshe/booruchan/boors/entity/Post.kt
@@ -2,30 +2,82 @@ package com.makentoshe.booruchan.boors.entity
 
 import java.io.Serializable
 
-abstract class Post: Serializable{
+abstract class Post: Serializable {
 
-    lateinit var sample_url: String
+    enum class Rating {
+        SAFE, QUESTIONABLE, EXPLICIT, WTF
+    }
+
+    lateinit var sampleUrl: String
 
     lateinit var tags: Array<String>
 
-    lateinit var preview_url: String
+    lateinit var previewUrl: String
 
-    lateinit var file_url: String
+    lateinit var fileUrl: String
 
-    lateinit var id: String
+    var id: Int = -1
 
-    lateinit var created_at: String
+    lateinit var createdAt: String
 
-    lateinit var creator_id: String
+    var creatorId: Int = -1
 
-    lateinit var rating: String
+    lateinit var rating: Rating
 
-    lateinit var score: String
+    var score: Int = 0
 
     lateinit var raw: Map<String, String>
 
-    lateinit var has_comments: String
+    var hasComments: Boolean = false
 
-    abstract fun fill(attributes: Map<String, String>)
+    open fun fill(attributes: Map<String, String>) {
+        raw = attributes
+        //create Entry
+        val entrySet = attributes.entries
+        //for each attribute
+        for ((key, value) in entrySet) {
+            when (key) {
+                "id" -> {
+                    id = value.toInt()
+                }
+                "rating" -> {
+                    rating = parseRating(value)
+                }
+                "score" -> {
+                    score = value.toInt()
+                }
+                "preview_url" -> {
+                    previewUrl = value
+                }
+                "tags" -> {
+                    tags = value.split(" ").toTypedArray()
+                }
+                "sample_url" -> {
+                    sampleUrl = value
+                }
+                "file_url" -> {
+                    fileUrl = value
+                }
+                "creator_id" -> {
+                    creatorId = value.toInt()
+                }
+                "has_comments" -> {
+                    hasComments = value.toBoolean()
+                }
+                "created_at" -> {
+                    createdAt = value
+                }
+            }
+        }
+    }
+
+    private fun parseRating(str: String): Rating {
+        return when (str) {
+            "s" -> Rating.SAFE
+            "q" -> Rating.QUESTIONABLE
+            "e" -> Rating.EXPLICIT
+            else -> Rating.WTF
+        }
+    }
 
 }
