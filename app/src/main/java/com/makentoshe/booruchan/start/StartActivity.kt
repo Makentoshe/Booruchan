a5a19b8e3d0224731d9ae279e294becdbcc2e8ec
Makentoshe <mkliverout@gmail.com>
2018-09-04 17:43:56 +0300

    initial commit

---
+++ app/src/main/java/com/makentoshe/booruchan/start/StartActivity.kt
@@ -0,0 +1,13 @@
+package com.makentoshe.booruchan.start
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.makentoshe.booruchan.R
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
