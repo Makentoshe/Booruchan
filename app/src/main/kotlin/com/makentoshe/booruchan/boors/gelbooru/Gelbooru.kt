96ff1c3357be2ec710ac1e27aa8002f6a60509ad
Makentoshe <mkliverout@gmail.com>
2018-09-05 20:32:03 +0300

    Add specified post class for gelbooru service.

--- app/src/main/kotlin/com/makentoshe/booruchan/boors/gelbooru/Gelbooru.kt
+++ app/src/main/kotlin/com/makentoshe/booruchan/boors/gelbooru/Gelbooru.kt
@@ -1,6 +1,7 @@
 package com.makentoshe.booruchan.boors.gelbooru
 
 import com.makentoshe.booruchan.boors.Boor
+import com.makentoshe.booruchan.boors.entity.Post
 
 class Gelbooru: Boor(GelbooruRequestAPI()) {
 
@@ -24,5 +25,88 @@ class Gelbooru: Boor(GelbooruRequestAPI()) {
 //        return@runBlocking parseJsonForAutocomplete(
 //                Scanner(async.await()).useDelimiter("\\A").next())
 //    }
+
+    class Post: com.makentoshe.booruchan.boors.entity.Post() {
+
+        var previewHeight: Int = -1
+
+        var previewWidth: Int = -1
+
+        lateinit var status: String
+
+        var sampleWidth: Int = -1
+
+        lateinit var change: String
+
+        var width: Int = -1
+
+        var hasNotes: Boolean = false
+
+        var sampleHeight: Int = -1
+
+        var height: Int = -1
+
+        var hasChildren: Boolean = false
+
+        lateinit var source: String
+
+        lateinit var md5: String
+
+        var parentId: Int = -1
+
+        override fun fill(attributes: Map<String, String>) {
+            super.fill(attributes)
+            //create Entry
+            val entrySet = attributes.entries
+            //for each attribute
+            for ((key, value) in entrySet) {
+                when (key) {
+                    "md5" -> {
+                        md5 = value
+                    }
+                    "source" -> {
+                        source = value
+                    }
+                    "height" -> {
+                        height = value.toInt()
+                    }
+                    "width" -> {
+                        width = value.toInt()
+                    }
+                    "sample_height" -> {
+                        sampleHeight = value.toInt()
+                    }
+                    "sample_width" -> {
+                        sampleWidth = value.toInt()
+                    }
+                    "preview_height" -> {
+                        previewHeight = value.toInt()
+                    }
+                    "preview_width" -> {
+                        previewWidth = value.toInt()
+                    }
+                    "has_notes" -> {
+                        hasNotes = value.toBoolean()
+                    }
+                    "status" -> {
+                        status = value
+                    }
+                    "change" -> {
+                        change = value
+                    }
+                    "has_children" -> {
+                        hasChildren = value.toBoolean()
+                    }
+                    "parent_id" -> {
+                        try{
+                            parentId = value.toInt()
+                        } catch (e: Exception){}
+                    }
+                }
+            }
+        }
+
+
+    }
 
 }
