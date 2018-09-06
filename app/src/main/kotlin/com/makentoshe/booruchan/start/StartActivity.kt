897820849c24a4332aedbe585e619ab588af47d7
Makentoshe <mkliverout@gmail.com>
2018-09-06 15:38:11 +0300

    Add toolbar to StartActivity

--- app/src/main/kotlin/com/makentoshe/booruchan/start/StartActivity.kt
+++ app/src/main/kotlin/com/makentoshe/booruchan/start/StartActivity.kt
@@ -2,12 +2,33 @@ package com.makentoshe.booruchan.start
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.makentoshe.booruchan.R
+import com.makentoshe.booruchan.StyleableAnkoComponent
+import com.makentoshe.booruchan.styles.AstarteStyle
 import org.jetbrains.anko.*
 
 class StartActivity : AppCompatActivity() {
 
+    val style = AstarteStyle()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        StartActivityView().setContentView(this)
+        StartActivityView(style).setContentView(this)
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.activity_start_overflow, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+//        when (item?.itemId) {
+//            R.id.action_app_settings -> {
+//                //do something
+//            }
+//        }
+//        return false
+//    }
 }
--- app/src/main/kotlin/com/makentoshe/booruchan/start/StartActivityView.kt
+++ app/src/main/kotlin/com/makentoshe/booruchan/start/StartActivityView.kt
@@ -1,14 +1,31 @@
 package com.makentoshe.booruchan.start
 
-import android.view.Gravity
+import android.support.v4.content.ContextCompat
+import com.makentoshe.booruchan.R
+import com.makentoshe.booruchan.StyleableAnkoComponent
+import com.makentoshe.booruchan.styles.Style
 import org.jetbrains.anko.*
+import org.jetbrains.anko.appcompat.v7.titleResource
+import org.jetbrains.anko.appcompat.v7.toolbar
 
-class StartActivityView : AnkoComponent<StartActivity> {
+class StartActivityView(style: Style) : StyleableAnkoComponent<StartActivity>(style) {
 
     override fun createView(ui: AnkoContext<StartActivity>) = with(ui) {
         verticalLayout {
-            padding = dip(8)
+
+            val toolbar = toolbar {
+                setTitleTextColor(ContextCompat.getColor(ui.ctx, style.toolbarTextColor))
+                id = R.id.activity_start_toolbar
+                titleResource = R.string.app_name
+                backgroundColorResource = style.toolbarBackgroundColor
+            }.lparams {
+                width = matchParent
+                height = dip(style.dpToolbarHeight)
+            }
+            ui.owner.setSupportActionBar(toolbar)
+
             textView {
+                padding = dip(8)
                 text = "Sup, its just a start"
             }
         }
