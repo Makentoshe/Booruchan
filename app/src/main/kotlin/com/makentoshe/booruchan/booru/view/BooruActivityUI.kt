07d5ec17b3a75806b09f43dd7f21bb79cf4ae45f
Makentoshe <mkliverout@gmail.com>
2018-09-09 17:37:23 +0300

    Add hamburger icon for drawable layout in BooruActivityUI

--- app/src/main/kotlin/com/makentoshe/booruchan/booru/view/BooruActivityUI.kt
+++ app/src/main/kotlin/com/makentoshe/booruchan/booru/view/BooruActivityUI.kt
@@ -1,24 +1,87 @@
 package com.makentoshe.booruchan.booru.view
 
+import android.os.Build
+import android.support.v4.content.ContextCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.Gravity
 import android.view.View
+import com.makentoshe.booruchan.R
 import com.makentoshe.booruchan.StyleableAnkoComponent
 import com.makentoshe.booruchan.styles.Style
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
+import org.jetbrains.anko.*
+import org.jetbrains.anko.appcompat.v7.toolbar
+import org.jetbrains.anko.support.v4._DrawerLayout
+import org.jetbrains.anko.support.v4.drawerLayout
 
-class BooruActivityUI(style: Style): StyleableAnkoComponent<BooruActivity>(style) {
+class BooruActivityUI(style: Style) : StyleableAnkoComponent<BooruActivity>(style) {
 
-    override fun createView(ui: AnkoContext<BooruActivity>): View = with(ui){
-        verticalLayout {
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
 
+    override fun createView(ui: AnkoContext<BooruActivity>): View = with(ui) {
+        drawerLayout = drawerLayout {
+            lparams(width = matchParent, height = matchParent)
+            createContentView()
+            createPanelView()
+        }
+        setupHamburgerIcon(ui)
+        return@with drawerLayout
+    }
+
+    private fun _DrawerLayout.createPanelView() {
+        frameLayout {
+            backgroundResource = style.backgroudColor
             textView {
-                text = "SAS"
+                text = "Panel"
             }
+        }.lparams {
+            width = dip(260)
+            height = matchParent
+            gravity = Gravity.START
+        }
 
+    }
+
+    private fun _DrawerLayout.createContentView() {
+        frameLayout {
+            createToolbar()
+            textView {
+                text = "Content"
+            }
+        }.lparams(matchParent, matchParent)
+    }
+
+    private fun _FrameLayout.createToolbar() {
+        toolbar = toolbar {
+            setTitleTextColor(ContextCompat.getColor(context, style.toolbarTextColor))
+            title = "Gelbooru"
+            backgroundColorResource = style.toolbarBackgroundColor
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                elevation = dip(4).toFloat()
+            }
+        }.lparams {
+            width = matchParent
+            height = dip(style.dpToolbarHeight)
         }
     }
 
-
+    private fun setupHamburgerIcon(ui: AnkoContext<BooruActivity>) {
+        ui.owner.apply {
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            val toggle = ActionBarDrawerToggle(ui.owner, drawerLayout, toolbar,
+                    R.string.drawer_open, R.string.drawer_close)
+            toggle.isDrawerIndicatorEnabled = true
+            toggle.drawerArrowDrawable.color = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                ui.ctx.resources.getColor(style.toolbarTextColor, theme)
+            } else {
+                ui.ctx.resources.getColor(style.toolbarTextColor)
+            }
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+        }
+    }
 
 }
