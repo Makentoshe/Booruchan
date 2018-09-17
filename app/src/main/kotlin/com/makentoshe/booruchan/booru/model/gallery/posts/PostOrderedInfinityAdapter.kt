35a9558693f2170d87454e8d6b00cff8daab4994
Makentoshe <mkliverout@gmail.com>
2018-09-17 16:00:26 +0300

    Add JobScheduler for post loadings with coroutines.

--- app/src/main/kotlin/com/makentoshe/booruchan/booru/model/gallery/posts/PostOrderedInfinityAdapter.kt
+++ app/src/main/kotlin/com/makentoshe/booruchan/booru/model/gallery/posts/PostOrderedInfinityAdapter.kt
@@ -4,14 +4,14 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import com.makentoshe.booruchan.booru.view.posts.PostOrderedInfinityAdapterViewHolderUI
+import kotlinx.coroutines.experimental.*
 import org.jetbrains.anko.*
+import java.util.*
 
 class PostOrderedInfinityAdapter(private val viewModel: PostOrderedInfinityViewModel, private val searchTerm: String)
     : RecyclerView.Adapter<PostOrderedInfinityAdapter.ViewHolder>() {
 
-    init {
-        println("Create with term $searchTerm")
-    }
+    private val jobScheduler = JobScheduler(10)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -23,9 +23,13 @@ class PostOrderedInfinityAdapter(private val viewModel: PostOrderedInfinityViewM
         return Int.MAX_VALUE
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        //todo implement this
-        println("Load row $position")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = runBlocking {
+        val job = GlobalScope.launch(Dispatchers.Default) {
+            //todo implement post loading here
+            delay(1000)
+            println("Load row $position")
+        }
+        jobScheduler.addJob(job)
     }
 
 
@@ -33,6 +37,19 @@ class PostOrderedInfinityAdapter(private val viewModel: PostOrderedInfinityViewM
 
         companion object {
             const val SIDE = 110
+        }
+
+    }
+
+    class JobScheduler(private val maxSize: Int) {
+
+        private val jobDeque = ArrayDeque<Job>()
+
+        fun addJob(job: Job) {
+            if (jobDeque.size >= maxSize) {
+                jobDeque.removeFirst().cancel()
+            }
+            jobDeque.addLast(job)
         }
 
     }
