aef01cf31bddc2b0bb8f7b48a6b4f9604cc48d26
Makentoshe <mkliverout@gmail.com>
2018-10-18 13:21:13 +0300

    Add tags selecting listener in chips builder.

--- app/src/main/kotlin/com/makentoshe/booruchan/sample/view/SampleActivityUI.kt
+++ app/src/main/kotlin/com/makentoshe/booruchan/sample/view/SampleActivityUI.kt
@@ -7,6 +7,7 @@ import android.graphics.drawable.GradientDrawable
 import android.support.design.chip.Chip
 import android.support.design.chip.ChipGroup
 import android.support.v4.content.ContextCompat
+import android.support.v4.util.ArraySet
 import android.support.v7.widget.Toolbar
 import android.text.TextUtils
 import android.view.Gravity
@@ -22,6 +23,7 @@ import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import org.jetbrains.anko.*
 import org.jetbrains.anko.appcompat.v7.toolbar
 import org.jetbrains.anko.cardview.v7.cardView
+import org.jetbrains.anko.sdk25.coroutines.onFocusChange
 import org.jetbrains.anko.support.v4.onPageChangeListener
 import org.jetbrains.anko.support.v4.viewPager
 
@@ -38,177 +40,207 @@ class SampleActivityUI(style: Style, private val viewModel: SampleViewModel)
         }
     }
 
-    interface ViewBuilder<View> {
-        fun build(ui: AnkoContext<SampleActivity>): View
-    }
+}
 
-    class ActionBarBuilder(private val style: Style) : ViewBuilder<Toolbar> {
+interface ViewBuilder<View> {
+    fun build(ui: AnkoContext<SampleActivity>): View
+}
 
-        override fun build(ui: AnkoContext<SampleActivity>): Toolbar = with(ui.ctx) {
-            return toolbar {
-                backgroundColorResource = style.toolbar.primaryColorRes
-                title = ""
-                setNavigationIcon(style.avdFromMenuToCross)
-                navigationIcon?.setColorFilter(
-                        ContextCompat.getColor(context, style.toolbar.onPrimaryColorRes),
-                        PorterDuff.Mode.SRC_ATOP)
-            }
+class ActionBarBuilder(private val style: Style) : ViewBuilder<Toolbar> {
+
+    override fun build(ui: AnkoContext<SampleActivity>): Toolbar = with(ui.ctx) {
+        return toolbar {
+            backgroundColorResource = style.toolbar.primaryColorRes
+            title = ""
+            setNavigationIcon(style.avdFromMenuToCross)
+            navigationIcon?.setColorFilter(
+                    ContextCompat.getColor(context, style.toolbar.onPrimaryColorRes),
+                    PorterDuff.Mode.SRC_ATOP)
         }
     }
+}
 
-    class BackdropViewBuilder(private val style: Style, private val viewModel: SampleViewModel)
-        : ViewBuilder<BackdropImpl> {
+class BackdropViewBuilder(private val style: Style, private val viewModel: SampleViewModel)
+    : ViewBuilder<BackdropImpl> {
 
-        override fun build(ui: AnkoContext<SampleActivity>): BackdropImpl = with(ui.ctx) {
-            return backdrop {
-                setGravity(Gravity.BOTTOM)
-                panelHeight = dip(style.toolbar.dpHeight)
-                coveredFadeColor = ContextCompat.getColor(context, android.R.color.transparent)
-                backgroundColorResource = style.backdrop.primaryColorRes
-                panelState = SlidingUpPanelLayout.PanelState.EXPANDED
-                isTouchEnabled = false
+    override fun build(ui: AnkoContext<SampleActivity>): BackdropImpl = with(ui.ctx) {
+        return backdrop {
+            setGravity(Gravity.BOTTOM)
+            panelHeight = dip(style.toolbar.dpHeight)
+            coveredFadeColor = ContextCompat.getColor(context, android.R.color.transparent)
+            backgroundColorResource = style.backdrop.primaryColorRes
+            panelState = SlidingUpPanelLayout.PanelState.EXPANDED
+            isTouchEnabled = false
 
-                addView(BackdropContentViewBuilder(style, viewModel).build(ui))
-                addView(BackdropPanelViewBuilder(style, viewModel).build(ui))
-            }
+            addView(BackdropContentViewBuilder(style, viewModel).build(ui))
+            addView(BackdropPanelViewBuilder(style, viewModel).build(ui))
         }
     }
+}
 
-    class BackdropContentViewBuilder(private val style: Style, private val viewModel: SampleViewModel)
-        : ViewBuilder<View> {
+class BackdropContentViewBuilder(private val style: Style, private val viewModel: SampleViewModel)
+    : ViewBuilder<View> {
 
-        @SuppressLint("NewApi")
-        override fun build(ui: AnkoContext<SampleActivity>): View = with(ui.ctx) {
-            return scrollView {
-                verticalLayout {
+    @SuppressLint("NewApi")
+    override fun build(ui: AnkoContext<SampleActivity>): View = with(ui.ctx) {
+        return scrollView {
+            verticalLayout {
 
-                    val id = textDataView(context.getString(R.string.id))
-                    separate()
-                    val time = textDataView(context.getString(R.string.time))
-                    separate()
-                    val rating = textDataView(context.getString(R.string.rating))
-                    separate()
-                    val source = textDataView(context.getString(R.string.source))
-                    separate()
-                    val chippedTags = ChippedTagsViewBuilder(style)
-                    addView(chippedTags.build(ui))
+                val id = textDataView(context.getString(R.string.id))
+                separate()
+                val time = textDataView(context.getString(R.string.time))
+                separate()
+                val rating = textDataView(context.getString(R.string.rating))
+                separate()
+                val source = textDataView(context.getString(R.string.source))
+                separate()
+                val chippedTags = ChippedTagsViewBuilder(style)
+                addView(chippedTags.build(ui))
 
-                    viewModel.setPostObserver(ui.owner) {
-                        id.text = StringBuilder(context.getString(R.string.id)).append(" ")
-                                .append(it.id)
-                        time.text = StringBuilder(context.getString(R.string.time)).append(" ")
-                                .append(viewModel.booru.convertLocalTimeToDefault(it.createdAt))
-                        rating.text = StringBuilder(context.getString(R.string.rating)).append(" ")
-                                .append(it.rating)
-                        source.text = StringBuilder(context.getString(R.string.source)).append(" ")
-                                .append(it.source)
-                        chippedTags.update(it.tags)
-                    }
+                viewModel.setPostObserver(ui.owner) {
+                    id.text = StringBuilder(context.getString(R.string.id)).append(" ")
+                            .append(it.id)
+                    time.text = StringBuilder(context.getString(R.string.time)).append(" ")
+                            .append(viewModel.booru.convertLocalTimeToDefault(it.createdAt))
+                    rating.text = StringBuilder(context.getString(R.string.rating)).append(" ")
+                            .append(it.rating)
+                    source.text = StringBuilder(context.getString(R.string.source)).append(" ")
+                            .append(it.source)
+                    chippedTags.update(it.tags)
                 }
             }
         }
+    }
 
-        private fun _LinearLayout.textDataView(text: String): TextView {
-            return textView {
-                this.text = text
-                textColor = ContextCompat.getColor(context, style.backdrop.onPrimaryColorRes)
-                linksClickable = true
-                maxLines = 1
-                ellipsize = TextUtils.TruncateAt.MARQUEE
-            }.lparams { setMargins(dip(16), 0, 0, 0) }
+    private fun _LinearLayout.textDataView(text: String): TextView {
+        return textView {
+            this.text = text
+            textColor = ContextCompat.getColor(context, style.backdrop.onPrimaryColorRes)
+            linksClickable = true
+            maxLines = 1
+            ellipsize = TextUtils.TruncateAt.MARQUEE
+        }.lparams { setMargins(dip(16), 0, 0, 0) }
+    }
+
+    private fun _LinearLayout.separate() {
+        cardView {
+            radius = dip(1).toFloat()
+            backgroundColorResource = style.chip.secondaryColorRes
+        }.lparams(Int.MAX_VALUE, dip(1)) {
+            setMargins(dip(16), 0, 0, dip(16))
         }
+    }
 
-        private fun _LinearLayout.separate() {
+}
+
+class BackdropPanelViewBuilder(private val style: Style, private val viewModel: SampleViewModel)
+    : ViewBuilder<View> {
+
+    @SuppressLint("NewApi")
+    override fun build(ui: AnkoContext<SampleActivity>): View = with(ui.ctx) {
+        val colors = arrayOf(
+                ContextCompat.getColor(this, android.R.color.transparent),
+                ContextCompat.getColor(this, style.view.primaryColorRes))
+        val gradient = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors.toIntArray())
+
+        verticalLayout {
+            backgroundDrawable = gradient
+            layoutParams = LinearLayout.LayoutParams(matchParent, matchParent)
+            forLollipop { elevation = 0f }
             cardView {
-                radius = dip(1).toFloat()
-                backgroundColorResource = style.chip.secondaryColorRes
-            }.lparams(Int.MAX_VALUE, dip(1)) {
-                setMargins(dip(16), 0, 0, dip(16))
+                radius = dip(16).toFloat()
+                layoutParams = ViewGroup.LayoutParams(matchParent, getScreenSize(context).height)
+
+                viewPager {
+                    id = R.id.sample_backdrop_sliding_viewpager
+                    adapter = ViewPagerAdapter(ui.owner.supportFragmentManager, viewModel)
+                    onPageChangeListener { onPageSelected { viewModel.setCurrentPage(currentItem) } }
+                    currentItem = viewModel.pageId
+                }
             }
         }
-
     }
+}
 
-    class BackdropPanelViewBuilder(private val style: Style, private val viewModel: SampleViewModel)
-        : ViewBuilder<View> {
+class ChippedTagsViewBuilder(private val style: Style) : ViewBuilder<View> {
 
-        @SuppressLint("NewApi")
-        override fun build(ui: AnkoContext<SampleActivity>): View = with(ui.ctx) {
-            val colors = arrayOf(
-                    ContextCompat.getColor(this, android.R.color.transparent),
-                    ContextCompat.getColor(this, style.view.primaryColorRes))
-            val gradient = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors.toIntArray())
+    private lateinit var container: ChipGroup
+    private val selectedTags = ArraySet<String>()
+    private var tagsListener: TagsSelectedListener? = null
 
-            verticalLayout {
-                backgroundDrawable = gradient
-                layoutParams = LinearLayout.LayoutParams(matchParent, matchParent)
-                forLollipop { elevation = 0f }
-                cardView {
-                    radius = dip(16).toFloat()
-                    layoutParams = ViewGroup.LayoutParams(matchParent, getScreenSize(context).height)
+    override fun build(ui: AnkoContext<SampleActivity>): View = with(ui.ctx) {
+        verticalLayout {
+            textView(R.string.tags) {
+                textColor = ContextCompat.getColor(context, style.backdrop.onPrimaryColorRes)
+            }.lparams { setMargins(dip(16), 0, 0, dip(8)) }
 
-                    viewPager {
-                        id = R.id.sample_backdrop_sliding_viewpager
-                        adapter = ViewPagerAdapter(ui.owner.supportFragmentManager, viewModel)
-                        onPageChangeListener { onPageSelected { viewModel.setCurrentPage(currentItem) } }
-                        currentItem = viewModel.pageId
+            container = chipGroup {
+                gravity = Gravity.FILL_HORIZONTAL
+            }.lparams { setMargins(dip(16), 0, 0, dip(16)) }
+
+
+            setOnTagsSelectedListener(object : ChippedTagsViewBuilder.TagsSelectedListener() {
+                override fun onTagSelected(tag: String, set: Set<String>) {
+                    println("Show icon")
+                    //show search icon
+                }
+
+                override fun onTagDeselected(tag: String, set: Set<String>) {
+                    if (set.isEmpty()) {
+                        println("Hide icon")
                     }
                 }
+            })
+        }
+    }
+
+    fun update(tags: Array<String>) {
+        tags.forEachIndexed { i, tag ->
+            val view = container.getChildAt(i)
+            if (view == null) {
+                container.addView(createChip(tag))
+            } else {
+                (view as Chip).text = tag
+            }
+        }
+        if (tags.size < container.childCount) {
+            for (i in tags.size until container.childCount step 1) {
+                container.getChildAt(i)?.visibility = View.GONE
             }
         }
     }
 
-    class ChippedTagsViewBuilder(private val style: Style) : ViewBuilder<View> {
-
-        private lateinit var container: ChipGroup
-
-        override fun build(ui: AnkoContext<SampleActivity>): View = with(ui.ctx) {
-            verticalLayout {
-                textView(R.string.tags) {
-                    textColor = ContextCompat.getColor(context, style.backdrop.onPrimaryColorRes)
-                }.lparams { setMargins(dip(16), 0, 0, dip(8)) }
-
-                container = chipGroup {
-                    gravity = Gravity.FILL_HORIZONTAL
-                }.lparams { setMargins(dip(16), 0, 0, dip(16)) }
+    private fun createChip(tag: String) = Chip(container.context).apply {
+        text = tag
+        isClickable = true
+        setChipBackgroundColorResource(style.chip.primaryColorRes)
+        textColorResource = style.chip.onPrimaryColorRes
+        isCheckable = true
+        isCheckedIconVisible = false
+        setOnCheckedChangeListener { chip, checked ->
+            if (checked) {
+                setChipBackgroundColorResource(style.chip.secondaryColorRes)
+                textColorResource = style.chip.onSecondaryColorRes
+                selectedTags.add(tag)
+                tagsListener?.onTagSelected(tag, selectedTags)
+            } else {
+                setChipBackgroundColorResource(style.chip.primaryColorRes)
+                textColorResource = style.chip.onPrimaryColorRes
+                selectedTags.remove(tag)
+                tagsListener?.onTagDeselected(tag, selectedTags)
             }
         }
-
-        fun update(tags: Array<String>) {
-            tags.forEachIndexed { i, tag ->
-                val view = container.getChildAt(i)
-                if (view == null) {
-                    container.addView(createChip(tag))
-                } else {
-                    (view as Chip).text = tag
-                }
-            }
-            if (tags.size < container.childCount) {
-                for (i in tags.size until container.childCount step 1) {
-                    container.getChildAt(i)?.visibility = View.GONE
-                }
-            }
-        }
-
-        private fun createChip(tag: String) = Chip(container.context).apply {
-            text = tag
-            isClickable = true
-            setChipBackgroundColorResource(style.chip.primaryColorRes)
-            textColorResource = style.chip.onPrimaryColorRes
-            isCheckable = true
-            isCheckedIconVisible = false
-            setOnCheckedChangeListener { chip, checked ->
-                if (checked) {
-                    setChipBackgroundColorResource(style.chip.secondaryColorRes)
-                    textColorResource = style.chip.onSecondaryColorRes
-                } else {
-                    setChipBackgroundColorResource(style.chip.primaryColorRes)
-                    textColorResource = style.chip.onPrimaryColorRes
-                }
-                println("$tag\t$checked")
-            }
-        }
-
-
     }
+
+    fun setOnTagsSelectedListener(l: TagsSelectedListener?) {
+        tagsListener = l
+    }
+
+    abstract class TagsSelectedListener {
+
+        open fun onTagSelected(tag: String, set: Set<String>) = Unit
+
+        open fun onTagDeselected(tag: String, set: Set<String>) = Unit
+    }
+
 }
