97da8967b6774ad9fdae6df9a82efb327e1306f6
Makentoshe <mkliverout@gmail.com>
2018-12-13 22:36:41 +0300

    [feature] Add Posts and post classes

---
+++ BooruAPI/src/main/java/com/makentoshe/booruapi/Post.kt
@@ -0,0 +1,103 @@
+package com.makentoshe.booruapi
+
+import java.io.Serializable
+import java.lang.StringBuilder
+
+data class Post(
+    @JvmField val raw: Map<String, String>
+) : Serializable {
+
+    constructor(
+        id: Int = -1,
+        score: Int = 0,
+        previewUrl: String = "",
+        sampleUrl: String = "",
+        fileUrl: String = "",
+        creatorId: Int = -1,
+        rating: Rating = Rating.WTF,
+        tags: Array<Tag> = arrayOf()
+    ) : this(HashMap<String, String>().apply {
+        put("id", id.toString())
+        put("score", score.toString())
+        put("preview_url", previewUrl)
+        put("sample_url", sampleUrl)
+        put("file_url", fileUrl)
+        put("creator_id", creatorId.toString())
+        put("rating", rating.name.toLowerCase().first().toString())
+        put("tags", StringBuilder("").apply {
+            tags.forEach { append(it.name).append(" ") }
+        }.toString())
+    })
+
+    enum class Rating {
+        SAFE, QUESTIONABLE, EXPLICIT, WTF
+    }
+
+    @JvmField
+    var id = -1
+
+    @JvmField
+    var score = 0
+
+    lateinit var previewUrl: String
+
+    lateinit var sampleUrl: String
+
+    lateinit var fileUrl: String
+
+    @JvmField
+    var creatorId = -1
+
+    lateinit var rating: Rating
+
+    lateinit var tags: Array<Tag>
+
+    init {
+        for (entry in raw.entries) {
+            when (entry.key) {
+                "id" -> {
+                    id = entry.value.toInt()
+                }
+                "score" -> {
+                    score = entry.value.toInt()
+                }
+                "preview_url" -> {
+                    previewUrl = entry.value
+                }
+                "sample_url" -> {
+                    sampleUrl = entry.value
+                }
+                "file_url" -> {
+                    fileUrl = entry.value
+                }
+                "creator_id" -> {
+                    creatorId = entry.value.toInt()
+                }
+                "rating" -> {
+                    rating = parseRating(entry.value)
+                }
+                "tags" -> {
+                    tags = parseTags(entry.value)
+                }
+            }
+        }
+    }
+
+    private fun parseRating(str: String): Rating {
+        return when (str) {
+            "s" -> Rating.SAFE
+            "q" -> Rating.QUESTIONABLE
+            "e" -> Rating.EXPLICIT
+            else -> Rating.WTF
+        }
+    }
+
+    private fun parseTags(str: String): Array<Tag> {
+        val stags = str.split(" ").toTypedArray()
+        return Array(stags.size) {
+            val map = HashMap<String, String>()
+            map["name"] = stags[it]
+            return@Array Tag(map)
+        }
+    }
+}
---
+++ BooruAPI/src/main/java/com/makentoshe/booruapi/Posts.kt
@@ -0,0 +1,30 @@
+package com.makentoshe.booruapi
+
+class Posts(
+    posts: List<Post>,
+    val count: Int = -1,
+    val offset: Int = 0
+): ArrayList<Post>()  {
+    init {
+        addAll(posts)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (!super.equals(other)) return false
+
+        other as Posts
+
+        if (count != other.count) return false
+        if (offset != other.offset) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + count
+        result = 31 * result + offset
+        return result
+    }
+}
