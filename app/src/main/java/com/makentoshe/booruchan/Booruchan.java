50b79b00253b275cd6d179b134cd6fdce00f3eae
Makentoshe <mkliverout@gmail.com>
2018-12-30 18:57:11 +0300

    [feature] Add custom Application class

---
+++ app/src/main/java/com/makentoshe/booruchan/Booruchan.java
@@ -0,0 +1,53 @@
+package com.makentoshe.booruchan;
+
+
+import android.app.Application;
+import com.makentoshe.booruapi.Booru;
+import com.makentoshe.booruapi.Gelbooru;
+import com.makentoshe.network.HttpClient;
+import com.makentoshe.network.fuel.FuelClientFactory;
+import ru.terrakok.cicerone.Cicerone;
+import ru.terrakok.cicerone.NavigatorHolder;
+import ru.terrakok.cicerone.Router;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Booruchan extends Application {
+
+    public static Booruchan INSTANCE;
+    private Cicerone<Router> cicerone;
+    private Style style;
+    private List<Booru> boorus = new ArrayList<>();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        INSTANCE = this;
+        cicerone = Cicerone.create();
+        load();
+    }
+
+    private void load() {
+        style = new SotisStyle();
+        HttpClient client = new FuelClientFactory().buildClient();
+        boorus.add(new Gelbooru(client));
+    }
+
+    public NavigatorHolder getNavigatorHolder() {
+        return cicerone.getNavigatorHolder();
+    }
+
+    public Router getRouter() {
+        return cicerone.getRouter();
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public List<Booru> getBoorus() {
+        return boorus;
+    }
+}
+
