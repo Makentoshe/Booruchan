3d4a73ed1fa841328a7cd4b62175a2db1f562147
Makentoshe <mkliverout@gmail.com>
2018-12-30 18:33:36 +0300

    [feature] Add Cicerone navigation

---
+++ app/src/main/java/com/makentoshe/booruchan/Navigator.kt
@@ -0,0 +1,238 @@
+package com.makentoshe.booruchan
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import ru.terrakok.cicerone.Navigator
+import ru.terrakok.cicerone.commands.*
+import java.util.*
+
+open class Navigator(
+    private val activity: FragmentActivity,
+    private val containerId: Int,
+    private val fragmentManager: FragmentManager = activity.supportFragmentManager
+) : Navigator {
+
+    private var localStackCopy: LinkedList<String>? = null
+
+    override fun applyCommands(commands: Array<Command>) {
+        fragmentManager.executePendingTransactions()
+
+        //copy stack before apply commands
+        copyStackToLocal()
+
+        for (command in commands) {
+            applyCommand(command)
+        }
+    }
+
+    private fun copyStackToLocal() {
+        localStackCopy = LinkedList()
+
+        val stackSize = fragmentManager.backStackEntryCount
+        for (i in 0 until stackSize) {
+            localStackCopy!!.add(fragmentManager.getBackStackEntryAt(i).name!!)
+        }
+    }
+
+    /**
+     * Perform transition described by the navigation command
+     *
+     * @param command the navigation command to apply
+     */
+    protected open fun applyCommand(command: Command) {
+        when (command) {
+            is Forward -> activityForward(command)
+            is Replace -> activityReplace(command)
+            is BackTo -> backTo(command)
+            is Back -> fragmentBack()
+        }
+    }
+
+    protected open fun activityForward(command: Forward) {
+        val screen = command.screen as Screen
+        val activityIntent = screen.activityIntent
+
+        // Start activity
+        if (activityIntent != null) {
+            val options = createStartActivityOptions(command, activityIntent)
+            checkAndStartActivity(screen, activityIntent, options)
+        } else {
+            fragmentForward(command)
+        }
+    }
+
+    protected open fun fragmentForward(command: Forward) {
+        val screen = command.screen as Screen
+        val fragment = createFragment(screen)
+
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        setupFragmentTransaction(
+            command,
+            fragmentManager.findFragmentById(containerId)!!,
+            fragment,
+            fragmentTransaction
+        )
+
+        fragmentTransaction
+            .replace(containerId, fragment)
+            .addToBackStack(screen.screenKey)
+            .commit()
+        localStackCopy!!.add(screen.screenKey)
+    }
+
+    protected open fun fragmentBack() {
+        if (localStackCopy!!.size > 0) {
+            fragmentManager.popBackStack()
+            localStackCopy!!.removeLast()
+        } else {
+            activityBack()
+        }
+    }
+
+    protected open fun activityBack() = activity.finish()
+
+    protected open fun activityReplace(command: Replace) {
+        val screen = command.screen as Screen
+        val activityIntent = screen.activityIntent
+
+        // Replace activity
+        if (activityIntent != null) {
+            val options = createStartActivityOptions(command, activityIntent)
+            checkAndStartActivity(screen, activityIntent, options)
+            activity.finish()
+        } else {
+            fragmentReplace(command)
+        }
+    }
+
+    protected open fun fragmentReplace(command: Replace) {
+        val screen = command.screen as Screen
+        val newFragment = createFragment(screen)
+
+        if (localStackCopy!!.size > 0) {
+            fragmentManager.popBackStack()
+            localStackCopy!!.removeLast()
+
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            setupFragmentTransaction(
+                command,
+                fragmentManager.findFragmentById(containerId),
+                newFragment,
+                fragmentTransaction
+            )
+
+            fragmentTransaction
+                .replace(containerId, newFragment)
+                .addToBackStack(screen.screenKey)
+                .commit()
+            localStackCopy!!.add(screen.screenKey)
+
+        } else {
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            setupFragmentTransaction(
+                command,
+                fragmentManager.findFragmentById(containerId),
+                newFragment,
+                fragmentTransaction
+            )
+
+            fragmentTransaction
+                .replace(containerId, newFragment)
+                .commit()
+        }
+    }
+
+    /**
+     * Performs [BackTo] command transition
+     */
+    protected open fun backTo(command: BackTo) {
+        if (command.screen == null) {
+            backToRoot()
+        } else {
+            val key = command.screen.screenKey
+            val index = localStackCopy!!.indexOf(key)
+            val size = localStackCopy!!.size
+
+            if (index != -1) {
+                for (i in 1 until size - index) {
+                    localStackCopy!!.removeLast()
+                }
+                fragmentManager.popBackStack(key, 0)
+            } else {
+                backToUnexisting(command.screen as Screen)
+            }
+        }
+    }
+
+    private fun backToRoot() {
+        fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        localStackCopy!!.clear()
+    }
+
+    /**
+     * Override this method to setup fragment transaction [FragmentTransaction].
+     * For example: setCustomAnimations(...), addSharedElement(...) or setReorderingAllowed(...)
+     *
+     * @param command             current navigation command. Will be only [Forward] or [Replace]
+     * @param currentFragment     current fragment in container
+     * (for [Replace] command it will be screen previous in new chain, NOT replaced screen)
+     * @param nextFragment        next screen fragment
+     * @param fragmentTransaction fragment transaction
+     */
+    protected open fun setupFragmentTransaction(
+        command: Command,
+        currentFragment: Fragment?,
+        nextFragment: Fragment?,
+        fragmentTransaction: FragmentTransaction
+    ) = Unit
+
+    /**
+     * Override this method to create option for start activity
+     *
+     * @param command        current navigation command. Will be only [Forward] or [Replace]
+     * @param activityIntent activity intent
+     * @return transition options
+     */
+    protected open fun createStartActivityOptions(command: Command, activityIntent: Intent): Bundle? = null
+
+    private fun checkAndStartActivity(screen: Screen, activityIntent: Intent, options: Bundle?) {
+        // Check if we can start activity
+        if (activityIntent.resolveActivity(activity.packageManager) != null) {
+            activity.startActivity(activityIntent, options)
+        } else {
+            unexistingActivity(screen, activityIntent)
+        }
+    }
+
+    /**
+     * Called when there is no activity to open `screenKey`.
+     *
+     * @param screen         screen
+     * @param activityIntent intent passed to start Activity for the `screenKey`
+     */
+    // Do nothing by default
+    protected open fun unexistingActivity(screen: Screen, activityIntent: Intent) = Unit
+
+    /**
+     * Creates Fragment matching `screenKey`.
+     *
+     * @param screen screen
+     * @return instantiated fragment for the passed screen
+     */
+    protected open fun createFragment(screen: Screen) = screen.fragment
+
+    /**
+     * Called when we tried to fragmentBack to some specific screen (via [BackTo] command),
+     * but didn't found it.
+     *
+     * @param screen screen
+     */
+    protected open fun backToUnexisting(screen: Screen) = backToRoot()
+}
