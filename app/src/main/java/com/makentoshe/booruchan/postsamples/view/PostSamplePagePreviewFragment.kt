4607e8e6bd54ee4a6e558cb3f26727d987e4aadc
Makentoshe <mkliverout@gmail.com>
2019-01-15 16:25:31 +0300

    [feature](sample) Add PostSamplePagePreviewFragment

---
+++ app/src/main/java/com/makentoshe/booruchan/postsamples/view/PostSamplePagePreviewFragment.kt
@@ -0,0 +1,16 @@
+package com.makentoshe.booruchan.postsamples.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class PostSamplePagePreviewFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return TextView(context).apply {
+            text = "preview"
+        }
+    }
+}
