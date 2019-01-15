f3bf2ab3f4e8a9f3d1ea49bca550b1fe7756322c
Makentoshe <mkliverout@gmail.com>
2019-01-15 16:36:27 +0300

    [feature](sample) Add SamplePageContrller. Add #getPagerAdapter(FragmentManager): PagerAdapter

--- app/src/main/java/com/makentoshe/booruchan/postsamples/PostsSampleFragmentViewModel.kt
+++ app/src/main/java/com/makentoshe/booruchan/postsamples/PostsSampleFragmentViewModel.kt
@@ -1,5 +1,6 @@
 package com.makentoshe.booruchan.postsamples
 
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModel
 import androidx.viewpager.widget.PagerAdapter
 import com.makentoshe.booruapi.Booru
@@ -8,12 +9,16 @@ import com.makentoshe.booruapi.Tag
 import com.makentoshe.booruchan.BooruScreen
 import com.makentoshe.booruchan.Booruchan
 import com.makentoshe.booruchan.posts.model.PostsRepository
+import com.makentoshe.booruchan.postsamples.model.SamplePageController
+import com.makentoshe.booruchan.postsamples.model.SamplePagePagerAdapter
 
 class PostsSampleFragmentViewModel(
     private val booru: Booru,
     val startPosition: Int,
     private val postsRepository: PostsRepository
 ) : ViewModel() {
+
+    val blockController = SamplePageController()
 
     /**
      * @param position post index start from 0.
@@ -28,7 +33,14 @@ class PostsSampleFragmentViewModel(
         Booruchan.INSTANCE.router.newChain(BooruScreen(booru, set))
     }
 
-//    fun getPagerAdapter(): PagerAdapter {
-//
-//    }
+    fun getPagerAdapter(fragmentManager: FragmentManager): PagerAdapter {
+        return SamplePagePagerAdapter(fragmentManager, blockController)
+    }
+
+    fun update() {
+        blockController.update()
+    }
+
+    fun backToPreviews() = Booruchan.INSTANCE.router.exit()
+
 }
