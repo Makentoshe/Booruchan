7999408df3665cf96ce415efc6147921cee86ec4
Makentoshe <mkliverout@gmail.com>
2019-01-26 17:59:36 +0300

    [feature](sample) Add background color for PostSamplePageInfoFragment

--- app/src/main/java/com/makentoshe/booruchan/postsamplespageinfo/PostSamplePageInfoFragment.kt
+++ app/src/main/java/com/makentoshe/booruchan/postsamplespageinfo/PostSamplePageInfoFragment.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.makentoshe.booruchan.Booruchan
+import org.jetbrains.anko.backgroundColorResource
 
-class PostSamplePageInfoFragment: Fragment() {
+class PostSamplePageInfoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return TextView(context).apply {
             text = "Info"
+            backgroundColorResource = Booruchan.INSTANCE.style.background.backgroundColorRes
         }
     }
 }
