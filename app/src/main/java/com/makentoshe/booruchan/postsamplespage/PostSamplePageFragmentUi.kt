422cde96c9a1721af21618d0b7a1520b7381b61e
Makentoshe <mkliverout@gmail.com>
2019-01-26 18:03:36 +0300

    [feature](sample) Update close/hide animation when swipe down

--- app/src/main/java/com/makentoshe/booruchan/postsamplespage/PostSamplePageFragmentUi.kt
+++ app/src/main/java/com/makentoshe/booruchan/postsamplespage/PostSamplePageFragmentUi.kt
@@ -3,6 +3,7 @@ package com.makentoshe.booruchan.postsamplespage
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewManager
+import androidx.annotation.Px
 import com.makentoshe.booruchan.R
 import com.makentoshe.booruchan.VerticalViewPager
 import org.jetbrains.anko.AnkoComponent
@@ -15,15 +16,16 @@ class PostSamplePageFragmentUi(
     private val viewModel: PostSamplePageViewModel
 ) : AnkoComponent<PostSamplePageFragment> {
 
-    private lateinit var root: View
-
     override fun createView(ui: AnkoContext<PostSamplePageFragment>): View = with(ui) {
         frameLayout {
-            root = this
             lparams(matchParent, matchParent)
             verticalViewPager {
                 id = R.id.content_viewpager
                 adapter = viewModel.getViewPagerAdapter(ui.owner.childFragmentManager)
+                currentItem = 1
+
+                val root = ui.owner.parentFragment!!.view!!
+                onPageChange(root)
                 onPageSelected(::pageSelected)
             }
         }
@@ -34,15 +36,26 @@ class PostSamplePageFragmentUi(
     }
 
     private fun VerticalViewPager.onPageSelected(action: (Int) -> Unit) {
-        onPageChangeListener { onPageSelected(action) }
+        onPageChangeListener {
+            onPageSelected(action)
+        }
+    }
+
+    private fun VerticalViewPager.onPageChange(root: View) {
+        onPageChangeListener {
+            onPageScrolled { position, offset, offsetPx ->
+                if (position == 0) root.alpha = offset
+            }
+        }
     }
 
     private fun pageSelected(page: Int) {
         when (page) {
+            0 -> viewModel.backToPreviews()
             //can swap
-            0 -> viewModel.unblock()
+            1 -> viewModel.unblock()
             //can't swap
-            1 -> viewModel.block()
+            2 -> viewModel.block()
         }
     }
 }
