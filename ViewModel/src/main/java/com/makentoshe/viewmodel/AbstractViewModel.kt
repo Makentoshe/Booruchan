991efdf427bb7f2a7bafff2afefa11c6ffb14572
Makentoshe <mkliverout@gmail.com>
2019-02-06 18:42:08 +0300

    [feature] Create new module for default viewmodel configurations.

---
+++ ViewModel/src/main/java/com/makentoshe/viewmodel/AbstractViewModel.kt
@@ -0,0 +1,8 @@
+package com.makentoshe.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Default abstraction under the [ViewModel] class.
+ */
+abstract class AbstractViewModel: ViewModel()
