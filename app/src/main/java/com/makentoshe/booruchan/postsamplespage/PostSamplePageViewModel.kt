b41c93e0acc0cc5d45101fe689fc8d6ededb7721
Makentoshe <mkliverout@gmail.com>
2019-02-06 21:54:52 +0300

    [refactor] Update PostSamplePageViewModel using ViewModel module

--- app/src/main/java/com/makentoshe/booruchan/postsamplespage/PostSamplePageViewModel.kt
+++ app/src/main/java/com/makentoshe/booruchan/postsamplespage/PostSamplePageViewModel.kt
@@ -1,6 +1,8 @@
 package com.makentoshe.booruchan.postsamplespage
 
 import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager.widget.PagerAdapter
 import com.makentoshe.booruchan.FragmentViewModel
 import com.makentoshe.booruchan.postsamples.model.SamplePageContentPagerAdapter
@@ -8,20 +10,39 @@ import com.makentoshe.booruchan.postsamples.model.SamplePageHorizontalScrollBloc
 import com.makentoshe.repository.ImageRepository
 import com.makentoshe.repository.PostsRepository
 
-class PostSamplePageViewModel(
-    private val position: Int,
-    private val samplePageHorizontalScrollBlockController: SamplePageHorizontalScrollBlockController,
-    private val sampleRepository: ImageRepository,
-    private val postsRepository: PostsRepository
-) : FragmentViewModel() {
+class PostSamplePageViewModel : com.makentoshe.viewmodel.ViewModel() {
+    private var position: Int = -1
+    private lateinit var samplePageHorizontalScrollBlockController: SamplePageHorizontalScrollBlockController
+    private lateinit var sampleRepository: ImageRepository
+    private lateinit var postsRepository: PostsRepository
 
-    fun block() = samplePageHorizontalScrollBlockController.newState(SamplePageHorizontalScrollBlockController.Command.BLOCK)
+    fun block() =
+        samplePageHorizontalScrollBlockController.newState(SamplePageHorizontalScrollBlockController.Command.BLOCK)
 
-    fun unblock() = samplePageHorizontalScrollBlockController.newState(SamplePageHorizontalScrollBlockController.Command.UNBLOCK)
+    fun unblock() =
+        samplePageHorizontalScrollBlockController.newState(SamplePageHorizontalScrollBlockController.Command.UNBLOCK)
 
-    fun backToPreviews() = samplePageHorizontalScrollBlockController.newState(SamplePageHorizontalScrollBlockController.Command.CLOSE)
+    fun backToPreviews() =
+        samplePageHorizontalScrollBlockController.newState(SamplePageHorizontalScrollBlockController.Command.CLOSE)
 
     fun getViewPagerAdapter(fragmentManager: FragmentManager): PagerAdapter {
         return SamplePageContentPagerAdapter(fragmentManager, sampleRepository, position, postsRepository)
     }
+
+    class Factory(
+        private val position: Int,
+        private val samplePageHorizontalScrollBlockController: SamplePageHorizontalScrollBlockController,
+        private val sampleRepository: ImageRepository,
+        private val postsRepository: PostsRepository
+    ) : ViewModelProvider.NewInstanceFactory() {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            val viewModel = PostSamplePageViewModel()
+            viewModel.position = position
+            viewModel.samplePageHorizontalScrollBlockController = samplePageHorizontalScrollBlockController
+            viewModel.sampleRepository = sampleRepository
+            viewModel.postsRepository = postsRepository
+            return viewModel as T
+        }
+    }
+
 }
