1017367729091e8969859c4e88ae296892fc1c01
Makentoshe <mkliverout@gmail.com>
2019-02-10 14:46:57 +0300

    [feature] Make RxController Serializable

--- Controllers/src/main/java/com/makentoshe/controllers/RxController.kt
+++ Controllers/src/main/java/com/makentoshe/controllers/RxController.kt
@@ -1,8 +1,9 @@
 package com.makentoshe.controllers
 
 import io.reactivex.disposables.Disposable
+import java.io.Serializable
 
-interface RxController<P, A> {
+interface RxController<P, A>: Serializable {
     fun subscribe(action: (P) -> Unit): Disposable
 
     fun action(param: A)
