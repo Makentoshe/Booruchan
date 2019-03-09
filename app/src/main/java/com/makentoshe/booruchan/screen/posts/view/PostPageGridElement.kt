38eb9dbd4b565fe6dd77945644d165218bf055be
Makentoshe <mkliverout@gmail.com>
2019-03-09 16:14:53 +0300

    [refactor] Update grid element view

--- app/src/main/java/com/makentoshe/booruchan/screen/posts/view/PostPageGridElement.kt
+++ app/src/main/java/com/makentoshe/booruchan/screen/posts/view/PostPageGridElement.kt
@@ -1,21 +1,20 @@
 package com.makentoshe.booruchan.screen.posts.view
 
 import android.content.Context
-import android.graphics.BitmapFactory
 import android.view.View
-import com.makentoshe.booruapi.Post
 import com.makentoshe.booruchan.R
-import com.makentoshe.booruchan.repository.RxRepository
-import io.reactivex.disposables.CompositeDisposable
 import org.jetbrains.anko.*
+import org.jetbrains.anko.cardview.v7.cardView
 import kotlin.random.Random
 
 class PostPageGridElement : AnkoComponent<Context> {
     override fun createView(ui: AnkoContext<Context>): View = with(ui) {
-        relativeLayout {
+        cardView {
+            radius = 0f
+            elevation = dip(4).toFloat()
             imageView {
+                setPadding(dip(4), dip(4), dip(4), dip(4))
                 id = R.id.posts_page_gridview_element_image
-                backgroundColor = Random.nextInt()
             }.lparams(matchParent, matchParent)
             lparams(dip(100), dip(100))
         }
--- app/src/main/java/com/makentoshe/booruchan/screen/posts/view/PostPageUiGrid.kt
+++ app/src/main/java/com/makentoshe/booruchan/screen/posts/view/PostPageUiGrid.kt
@@ -1,5 +1,6 @@
 package com.makentoshe.booruchan.screen.posts.view
 
+import android.view.Gravity
 import android.view.View
 import com.makentoshe.booruchan.R
 import org.jetbrains.anko.*
@@ -9,11 +10,16 @@ class PostPageUiGrid : AnkoComponent<_RelativeLayout> {
         gridView {
             id = R.id.posts_page_gridview
             visibility = View.GONE
+            gravity = Gravity.CENTER
+
             context.configuration(orientation = Orientation.PORTRAIT) {
+                topPadding = dip(12)
                 numColumns = 3
+                verticalSpacing = dip(10)
             }
             context.configuration(orientation = Orientation.LANDSCAPE) {
                 numColumns = 6
+                verticalSpacing = dip(10)
             }
         }.lparams(matchParent, matchParent) {
             centerInParent()
