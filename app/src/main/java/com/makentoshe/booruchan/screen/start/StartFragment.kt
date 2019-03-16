ca9dcecab0f3386ce724df781c93125e83ff4435
Makentoshe <mkliverout@gmail.com>
2019-03-16 14:30:30 +0300

    [refactor] Update overflow icon setup

--- app/src/main/java/com/makentoshe/booruchan/screen/start/StartFragment.kt
+++ app/src/main/java/com/makentoshe/booruchan/screen/start/StartFragment.kt
@@ -1,17 +1,17 @@
 package com.makentoshe.booruchan.screen.start
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import com.makentoshe.booruchan.Booruchan
+import com.makentoshe.booruchan.R
 import com.makentoshe.booruchan.router
 import com.makentoshe.booruchan.screen.start.inflator.StartUiInflatorContent
-import com.makentoshe.booruchan.screen.start.inflator.StartUiInflatorOverflow
 import com.makentoshe.booruchan.screen.start.model.StartScreenNavigator
 import com.makentoshe.booruchan.screen.start.view.StartFragmentUi
 import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.find
 
 class StartFragment : Fragment() {
 
@@ -29,7 +29,27 @@ class StartFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        StartUiInflatorOverflow(navigator).accept(view)
+        val icon = view.find<View>(R.id.start_toolbar_overflow)
+        icon.setOnClickListener(::onOverflowIconClick)
+
         StartUiInflatorContent(navigator, booruList).accept(view)
+    }
+
+
+    private fun onOverflowIconClick(view: View) {
+        val popupMenu = PopupMenu(view.context, view)
+        popupMenu.menu.add(Menu.NONE, R.id.settings, 1, view.context.getString(R.string.settings))
+        popupMenu.setOnMenuItemClickListener(::onOverflowItemMenuClick)
+        popupMenu.show()
+    }
+
+    private fun onOverflowItemMenuClick(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> {
+                navigator.navigateToSettingsScreen()
+                return true
+            }
+        }
+        return false
     }
 }
