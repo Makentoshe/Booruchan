72c000db5973217070c4cc80935c86b82bfcc245
Makentoshe <mkliverout@gmail.com>
2019-03-24 13:02:07 +0300

    [fix] Fix proxy decorator

--- app/src/main/java/com/makentoshe/booruchan/network/decorator/ProxyHttpClient.kt
+++ app/src/main/java/com/makentoshe/booruchan/network/decorator/ProxyHttpClient.kt
@@ -6,6 +6,7 @@ import com.makentoshe.booruchan.network.HttpResult
 import com.makentoshe.booruchan.network.fuel.FuelHttpResult
 
 class ProxyHttpClient(private val client: HttpClient, private val proxy: String) : HttpClient() {
+
     override fun get(url: String): HttpResult {
         val result = client.get(url)
         return if (!result.isSuccessful) {
@@ -17,7 +18,7 @@ class ProxyHttpClient(private val client: HttpClient, private val proxy: String)
     override fun post(url: String, body: ByteArray): HttpResult {
         val result = client.post(url, body)
         return if (!result.isSuccessful) {
-            val res = "http://service.bypass123.com/index.php".httpPost(listOf("url" to url)).body(body).response()
+            val res = proxy.httpPost(listOf("url" to url)).body(body).response()
             FuelHttpResult(res)
         } else result
     }
