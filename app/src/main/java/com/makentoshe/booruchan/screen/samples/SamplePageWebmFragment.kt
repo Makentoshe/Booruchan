3f14631fa42f16f3ea75955428bc3162e32def7f
Makentoshe <mkliverout@gmail.com>
2019-03-29 22:41:06 +0300

    [feature] Replace ExoPlayer by default android VideoView

--- app/src/main/java/com/makentoshe/booruchan/screen/samples/SamplePageWebmFragment.kt
+++ app/src/main/java/com/makentoshe/booruchan/screen/samples/SamplePageWebmFragment.kt
@@ -5,13 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.fragment.app.Fragment
-import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.source.MediaSource
-import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
 import com.makentoshe.booruchan.R
 import com.makentoshe.booruchan.api.Booru
 import com.makentoshe.booruchan.api.Post
@@ -23,13 +19,13 @@ import com.makentoshe.booruchan.repository.decorator.CachedRepository
 import com.makentoshe.booruchan.screen.samples.model.onError
 import com.makentoshe.booruchan.screen.samples.model.showOptionsList
 import com.makentoshe.booruchan.screen.samples.view.SamplePageWebmUi
-import com.makentoshe.booruchan.view.setGestureListener
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.find
+import org.jetbrains.anko.sdk27.coroutines.onLongClick
 
 class SamplePageWebmFragment : Fragment() {
 
@@ -57,35 +53,27 @@ class SamplePageWebmFragment : Fragment() {
         val disposable = Single.just(post)
             .subscribeOn(Schedulers.newThread())
             .map { booru.getCustom(mapOf("Range" to "bytes=0-1")).request(it.sampleUrl) }
-            .map { createMediaSource(it.url.toURI().toString()) }
+            .map { it.url.toURI().toString() }
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(::onSubscribe)
         disposables.add(disposable)
     }
 
-    private fun onSubscribe(mediaSource: MediaSource?, throwable: Throwable?) {
+    private fun onSubscribe(url: String?, throwable: Throwable?) {
         val pview = parentFragment!!.view!!
-        if (throwable == null) onSuccess(pview, mediaSource!!) else onError(pview, throwable)
+        if (throwable == null) onSuccess(pview, url!!) else onError(pview, throwable)
     }
 
-    private fun onSuccess(view: View, mediaSource: MediaSource) {
-        val playerview = view.findViewById<PlayerView>(R.id.samples_webm)
-        val exoPlayer = ExoPlayerFactory.newSimpleInstance(context).also { it.prepare(mediaSource) }
-        playerview.player = exoPlayer
+    private fun onSuccess(view: View, url: String) {
+        val videoView = view.findViewById<VideoView>(R.id.samples_webm)
+        //hide progress bar
         view.find<View>(R.id.samples_progress).visibility = View.GONE
-        playerview.visibility = View.VISIBLE
-        playerview.setGestureListener {
-            onDown { playerview.performClick() }
-            onLongPress { showOptionsList(booru, post) }
-        }
-    }
 
-    //create media source from url
-    private fun createMediaSource(url: String): MediaSource {
-        val uri = Uri.parse(url)
-        val useragent = Util.getUserAgent(requireContext(), requireContext().getString(R.string.app_name))
-        val dataSourceFactory = DefaultDataSourceFactory(requireContext(), useragent)
-        return ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
+        videoView.setVideoURI(Uri.parse(url))
+        videoView.setMediaController(MediaController(requireContext()))
+        videoView.requestFocus(0)
+        videoView.visibility = View.VISIBLE
+        videoView.onLongClick { showOptionsList(booru, post) }
     }
 
     companion object {
--- app/src/main/java/com/makentoshe/booruchan/screen/samples/view/SamplePageWebmUi.kt
+++ app/src/main/java/com/makentoshe/booruchan/screen/samples/view/SamplePageWebmUi.kt
@@ -1,22 +1,22 @@
 package com.makentoshe.booruchan.screen.samples.view
 
+import android.view.Gravity
 import android.view.View
-import android.view.ViewManager
 import androidx.fragment.app.Fragment
-import com.google.android.exoplayer2.ui.PlayerView
 import com.makentoshe.booruchan.R
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.custom.ankoView
+import org.jetbrains.anko.*
 
 class SamplePageWebmUi : AnkoComponent<Fragment> {
     override fun createView(ui: AnkoContext<Fragment>) = with(ui) {
-        playerView {
-            id = R.id.samples_webm
-            visibility = View.GONE
+        frameLayout {
+            lparams(matchParent, matchParent) { gravity = Gravity.CENTER }
+
+            videoView {
+                id = R.id.samples_webm
+                visibility = View.GONE
+            }.lparams(matchParent, matchParent) {
+                gravity = Gravity.CENTER
+            }
         }
     }
-
-    private fun ViewManager.playerView(init: PlayerView.() -> Unit): PlayerView =
-        ankoView({ PlayerView(it) }, 0, init)
 }
