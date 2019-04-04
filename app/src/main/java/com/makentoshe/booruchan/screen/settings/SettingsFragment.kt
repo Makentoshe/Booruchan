98d10423b15dd8e4ce05abc8c3c8d5d170ec233e
Makentoshe <mkliverout@gmail.com>
2019-04-05 00:43:20 +0300

    [refactor] Move StreamDownloadSettingsController to separate file

--- app/src/main/java/com/makentoshe/booruchan/screen/settings/SettingsFragment.kt
+++ app/src/main/java/com/makentoshe/booruchan/screen/settings/SettingsFragment.kt
@@ -5,12 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
 import androidx.fragment.app.Fragment
-import com.makentoshe.booruchan.R
 import com.makentoshe.booruchan.model.RequestCode
 import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.find
 
 class SettingsFragment : Fragment() {
 
@@ -36,39 +33,5 @@ class SettingsFragment : Fragment() {
         if (requestCode == RequestCode.settings) {
             nsfwSettingController.onResult(resultCode)
         }
-    }
-}
-
-class StreamDownloadSettingController(private val fragment: Fragment) {
-
-    private val checkbox by lazy {
-        fragment.view?.find<CheckBox>(R.id.setting_stream_download_checkbox)!!
-    }
-
-    private val view by lazy {
-        fragment.view?.find<View>(R.id.setting_stream_download)!!
-    }
-
-    private val context = fragment.requireContext()
-
-    fun onViewCreated() {
-        //set default setting
-        checkbox.isChecked = AppSettings.getStreamingDownload(context)
-        checkbox.setOnCheckedChangeListener { _, isChecked ->
-            onCheck(isChecked)
-        }
-        view.setOnClickListener {
-            checkbox.isChecked = AppSettings.getStreamingDownload(context).not()
-        }
-    }
-
-    private fun onCheck(isChecked: Boolean) = if (isChecked) check() else uncheck()
-
-    private fun check() {
-        AppSettings.setStreamingDownload(context, true)
-    }
-
-    private fun uncheck() {
-        AppSettings.setStreamingDownload(context, false)
     }
 }
