d83c9f1f1bdefbd0a95d2f4d797328418289616e
Makentoshe <mkliverout@gmail.com>
2019-04-06 20:18:13 +0300

    [feature] Add loading indicator for gif images

--- app/src/main/java/com/makentoshe/booruchan/screen/samples/SamplePageGifFragment.kt
+++ app/src/main/java/com/makentoshe/booruchan/screen/samples/SamplePageGifFragment.kt
@@ -9,14 +9,17 @@ import androidx.fragment.app.Fragment
 import com.makentoshe.booruchan.R
 import com.makentoshe.booruchan.api.Booru
 import com.makentoshe.booruchan.api.Post
+import com.makentoshe.booruchan.model.StreamDownloadListener
 import com.makentoshe.booruchan.model.arguments
-import com.makentoshe.booruchan.repository.SampleImageRepository
+import com.makentoshe.booruchan.repository.StreamDownloadRepository
 import com.makentoshe.booruchan.repository.cache.ImageInternalCache
 import com.makentoshe.booruchan.repository.cache.InternalCache
 import com.makentoshe.booruchan.repository.decorator.CachedRepository
+import com.makentoshe.booruchan.repository.decorator.StreamDownloadRepositoryDecoratorSample
 import com.makentoshe.booruchan.screen.samples.model.onError
 import com.makentoshe.booruchan.screen.samples.model.showOptionsList
 import com.makentoshe.booruchan.screen.samples.view.SamplePageGifUi
+import com.makentoshe.booruchan.view.CircularProgressBar
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -24,6 +27,7 @@ import io.reactivex.schedulers.Schedulers
 import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.find
 import org.jetbrains.anko.sdk27.coroutines.onLongClick
+import org.jetbrains.anko.support.v4.runOnUiThread
 import pl.droidsonroids.gif.GifDrawable
 import pl.droidsonroids.gif.GifImageView
 
@@ -39,9 +43,12 @@ class SamplePageGifFragment : Fragment() {
 
     private val samplesRepository by lazy {
         val cache = ImageInternalCache(requireContext(), InternalCache.Type.SAMPLE)
-        val source = SampleImageRepository(booru)
+        val streamSource = StreamDownloadRepository(streamListener, booru)
+        val source = StreamDownloadRepositoryDecoratorSample(streamSource)
         CachedRepository(cache, source)
     }
+
+    private val streamListener by lazy { StreamDownloadListener() }
 
     private val disposables = CompositeDisposable()
 
@@ -50,6 +57,11 @@ class SamplePageGifFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val progressview = parentFragment!!.view!!.find<CircularProgressBar>(R.id.samples_progress_concrete)
+        streamListener.onPartReceived { _, _, progress ->
+            runOnUiThread { progressview.setProgress((100 * progress).toInt()) }
+        }
+
         val disposable = Single.just(post)
             .subscribeOn(Schedulers.newThread())
             .map { samplesRepository.get(it) }
