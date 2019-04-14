02046021d718db3abb925ef40df671d39c95beb0
Makentoshe <mkliverout@gmail.com>
2019-04-15 02:40:42 +0300

    [feature] Add rule 34 booru. Add autocomplete, autocomplete tag parser and Tag interface implementation

---
+++ app/src/main/java/com/makentoshe/booruchan/api/rule34/Rule34.kt
@@ -0,0 +1,25 @@
+package com.makentoshe.booruchan.api.rule34
+
+import com.makentoshe.booruchan.api.*
+import com.makentoshe.booruchan.network.HttpClient
+import java.io.Serializable
+
+class Rule34(private val httpClient: HttpClient) : Booru, Serializable {
+    override val title = "Rule 34"
+
+    override fun getCustom(params: Map<String, String>) = GetCustom(httpClient, params)
+
+    override fun headCustom(params: Map<String, String>) = HeadCustom(httpClient, params)
+
+    override fun getAutocomplete() = Rule34Autocomplete(httpClient, getAutocompleteTagParser())
+
+    override fun getPosts(): Posts {
+        TODO("not implemented")
+    }
+
+    override fun getAutocompleteTagParser() = Rule34AutocompleteTagParser()
+
+    override fun getPostParser(): Parser<List<Post>> {
+        TODO("not implemented")
+    }
+}
---
+++ app/src/main/java/com/makentoshe/booruchan/api/rule34/Rule34Autocomplete.kt
@@ -0,0 +1,18 @@
+package com.makentoshe.booruchan.api.rule34
+
+import com.makentoshe.booruchan.api.Autocomplete
+import com.makentoshe.booruchan.api.Parser
+import com.makentoshe.booruchan.api.Tag
+import com.makentoshe.booruchan.network.HttpClient
+
+class Rule34Autocomplete(
+    private val httpClient: HttpClient,
+    private val tagParser: Parser<List<Tag>>
+) : Autocomplete {
+
+    override fun request(term: String): List<Tag> {
+        val url = "https://rule34.xxx/autocomplete.php?q=$term"
+        val stream = httpClient.get(url).stream
+        return tagParser.parse(stream)
+    }
+}
