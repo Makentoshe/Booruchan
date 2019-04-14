a212dd0e87ac6fc30c297ef5e3eb564b6da1bc6b
Makentoshe <mkliverout@gmail.com>
2019-04-15 02:11:09 +0300

    [refactor] Update tags type defining.

--- app/src/main/java/com/makentoshe/booruchan/api/safebooru/SafebooruTag.kt
+++ app/src/main/java/com/makentoshe/booruchan/api/safebooru/SafebooruTag.kt
@@ -1,6 +1,7 @@
 package com.makentoshe.booruchan.api.safebooru
 
 import com.makentoshe.booruchan.api.Tag
+import com.makentoshe.booruchan.api.Tag.Companion.defineTagType
 
 class SafebooruTag(
     override val raw: Map<String, String> = emptyMap(),
@@ -8,18 +9,5 @@ class SafebooruTag(
     override val id: Long = raw["id"]?.toLong() ?: -1L,
     override val count: Int = raw["count"]?.toInt() ?: -1,
     override val ambiguous: Boolean = raw["ambiguous"]?.toBoolean() ?: false,
-    override val type: Tag.Type = defineType(raw["type"])
-) : Tag {
-
-    companion object {
-        fun defineType(value: String?) = when (value) {
-            "general" -> Tag.Type.GENERAL
-            "artist" -> Tag.Type.ARTIST
-            "copyright" -> Tag.Type.COPYRIGHT
-            "character" -> Tag.Type.CHARACTER
-            "metadata" -> Tag.Type.METADATA
-            else -> Tag.Type.UNDEFINED
-        }
-    }
-
-}
+    override val type: Tag.Type = defineTagType(raw["type"])
+) : Tag
