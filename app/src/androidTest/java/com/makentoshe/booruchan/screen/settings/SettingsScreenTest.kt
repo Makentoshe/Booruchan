7925f1e9cf0cd8f3351af8e5fd9fbacc38c6ece8
Makentoshe <mkliverout@gmail.com>
2019-05-06 02:46:59 +0300

    [test] Add tests for SettingsScreen

---
+++ app/src/androidTest/java/com/makentoshe/booruchan/screen/settings/SettingsScreenTest.kt
@@ -0,0 +1,22 @@
+package com.makentoshe.booruchan.screen.settings
+
+import com.makentoshe.booruchan.screen.settings.fragment.SettingsFragment
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class SettingsScreenTest {
+
+    private lateinit var screen: SettingsScreen
+
+    @Before
+    fun init() {
+        screen = SettingsScreen()
+    }
+
+    @Test
+    fun shouldCreateSettingsFragment() {
+        assertEquals(SettingsFragment::class.java, screen.fragment::class.java)
+    }
+
+}
