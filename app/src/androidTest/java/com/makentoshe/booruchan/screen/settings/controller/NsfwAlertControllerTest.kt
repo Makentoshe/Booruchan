b81d6bf177e89854b46f38419c679131b996d066
Makentoshe <mkliverout@gmail.com>
2019-05-06 15:48:02 +0300

    [test] Update tests for NsfwAlertController

--- app/src/androidTest/java/com/makentoshe/booruchan/screen/settings/controller/NsfwAlertControllerTest.kt
+++ app/src/androidTest/java/com/makentoshe/booruchan/screen/settings/controller/NsfwAlertControllerTest.kt
@@ -1,5 +1,6 @@
 package com.makentoshe.booruchan.screen.settings.controller
 
+import android.content.pm.ActivityInfo
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -31,7 +32,7 @@ class NsfwAlertControllerTest {
         }
     }
 
-    @Test()
+    @Test(timeout = 5000)
     fun shouldReturnTrueOnPositiveButtonClick() {
         var result: Boolean? = null
         controller.showAlert { result = it }
@@ -59,6 +60,47 @@ class NsfwAlertControllerTest {
     fun shouldReturnFalseOnCancelOrOnDismiss() {
         var result: Boolean? = null
         controller.showAlert { result = it }
+        device.pressBack()
+        //wait while click button will be performed and lambda executed
+        while (result == null) Thread.yield()
+
+        assertEquals(false, result)
+    }
+
+    @Test(timeout = 5000)
+    fun shouldReturnTrueOnPositiveButtonClickAfterRotation() {
+        var result: Boolean? = null
+        controller.showAlert { result = it }
+        //rotate device
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        //click to the positive button
+        onView(withId(android.R.id.button1)).perform(click())
+        //wait while click button will be performed and lambda executed
+        while (result == null) Thread.yield()
+
+        assertEquals(true, result)
+    }
+
+    @Test(timeout = 5000)
+    fun shouldReturnFalseOnNegativeButtonClickAfterRotation() {
+        var result: Boolean? = null
+        controller.showAlert { result = it }
+        //rotate device
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        //click to the negative button
+        onView(withId(android.R.id.button2)).perform(click())
+        //wait while click button will be performed and lambda executed
+        while (result == null) Thread.yield()
+
+        assertEquals(false, result)
+    }
+
+    @Test(timeout = 5000)
+    fun shouldReturnFalseOnCancelOrOnDismissAfterRotation() {
+        var result: Boolean? = null
+        controller.showAlert { result = it }
+        //rotate device
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         device.pressBack()
         //wait while click button will be performed and lambda executed
         while (result == null) Thread.yield()
