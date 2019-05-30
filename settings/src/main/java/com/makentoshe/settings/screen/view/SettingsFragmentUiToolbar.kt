a995552780ad5b5a63d6de72fde48e0a647de5c1
Makentoshe <mkliverout@gmail.com>
2019-05-31 01:53:51 +0300

    [refactor] Remove styleAttr function and replace by default Anko's attr

--- settings/src/main/java/com/makentoshe/settings/screen/view/SettingsFragmentUiToolbar.kt
+++ settings/src/main/java/com/makentoshe/settings/screen/view/SettingsFragmentUiToolbar.kt
@@ -1,6 +1,5 @@
 package com.makentoshe.settings.screen.view
 
-import com.makentoshe.style.styleAttr
 import org.jetbrains.anko.*
 import org.jetbrains.anko.appcompat.v7.themedToolbar
 import org.jetbrains.anko.appcompat.v7.titleResource
@@ -8,7 +7,7 @@ import org.jetbrains.anko.appcompat.v7.titleResource
 class SettingsFragmentUiToolbar : AnkoComponent<_RelativeLayout> {
 
     override fun createView(ui: AnkoContext<_RelativeLayout>) = with(ui.owner) {
-        val style = styleAttr(com.makentoshe.style.R.attr.toolbar_style).data
+        val style = attr(com.makentoshe.style.R.attr.toolbar_style).data
         val height = dimen(com.makentoshe.style.R.dimen.toolbar_height)
         themedRelativeLayout(style) {
             id = com.makentoshe.settings.R.id.settings_toolbar
