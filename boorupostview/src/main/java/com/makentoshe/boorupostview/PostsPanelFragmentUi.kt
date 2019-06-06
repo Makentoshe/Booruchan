a6b6b03de27a2661fe9f6e8882488613cd59abfe
Makentoshe <mkliverout@gmail.com>
2019-06-06 21:49:18 +0300

    [feature] Add empry user interface for posts foreground content where a images will be placed

---
+++ boorupostview/src/main/java/com/makentoshe/boorupostview/PostsPanelFragmentUi.kt
@@ -0,0 +1,15 @@
+package com.makentoshe.boorupostview
+
+import android.content.Context
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.relativeLayout
+
+class PostsPanelFragmentUi : AnkoComponent<Context> {
+
+    override fun createView(ui: AnkoContext<Context>) = with(ui) {
+        relativeLayout {
+
+        }
+    }
+}
