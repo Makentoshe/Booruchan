3bf625b53d2fe256a8c71057d8da9b4af786e49a
Makentoshe <mkliverout@gmail.com>
2019-06-06 21:41:44 +0300

    [refactor] Update Material styles and layouts

--- style/src/main/java/com/makentoshe/style/AnkoLayouts.kt
+++ style/src/main/java/com/makentoshe/style/AnkoLayouts.kt
@@ -4,15 +4,19 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewManager
+import android.widget.AutoCompleteTextView
 import android.widget.FrameLayout
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
 import org.jetbrains.anko.custom.ankoView
+import org.jetbrains.anko.include
 
 fun ViewManager.materialButton(theme: Int = 0, init: AnkoMaterialButton.() -> Unit): AnkoMaterialButton {
     return ankoView({ AnkoMaterialButton(it) }, theme, init)
 }
 
 class AnkoMaterialButton(context: Context) : MaterialButton(context) {
+
     fun <T : View> T.lparams(
         width: Int = ViewGroup.LayoutParams.WRAP_CONTENT,
         height: Int = ViewGroup.LayoutParams.WRAP_CONTENT,
@@ -32,4 +36,11 @@ class AnkoMaterialButton(context: Context) : MaterialButton(context) {
         this@lparams.layoutParams = layoutParams
         return this
     }
+}
+
+fun ViewManager.materialEditText(init: TextInputLayout.(AutoCompleteTextView) -> Unit): TextInputLayout {
+    val textInputLayout = include<TextInputLayout>(com.makentoshe.style.R.layout.material_edit_text)
+    val editText = (textInputLayout.getChildAt(0) as ViewGroup).getChildAt(0) as AutoCompleteTextView
+    textInputLayout.init(editText)
+    return textInputLayout
 }
