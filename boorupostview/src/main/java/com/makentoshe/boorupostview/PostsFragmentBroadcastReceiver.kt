cc83820e86d88bee4e861ca3ffea7909a4bfb3b8
Makentoshe <mkliverout@gmail.com>
2019-06-15 14:16:46 +0300

    [refactor] Add static method for sending broadcast and registering receiver

--- boorupostview/src/main/java/com/makentoshe/boorupostview/PostsFragmentBroadcastReceiver.kt
+++ boorupostview/src/main/java/com/makentoshe/boorupostview/PostsFragmentBroadcastReceiver.kt
@@ -5,9 +5,14 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import com.makentoshe.boorulibrary.entitiy.Tag
 import com.makentoshe.boorupostview.listener.NewSearchStartedListener
+import java.io.Serializable
 
+/**
+ * Broadcast receiver for a new search event.
+ */
 class PostsFragmentBroadcastReceiver : BroadcastReceiver(), NewSearchStartedListener {
 
     private var listener: ((Set<Tag>) -> Unit)? = null
@@ -25,6 +30,22 @@ class PostsFragmentBroadcastReceiver : BroadcastReceiver(), NewSearchStartedList
 
     companion object {
         internal const val START_NEW_SEARCH = "NewSearchStarted"
+
+        fun sendBroadcast(context: Context, tags: Set<Tag>) {
+            val intent = Intent(START_NEW_SEARCH)
+            intent.putExtra(START_NEW_SEARCH, tags as Serializable)
+            context.sendBroadcast(intent)
+        }
+
+        fun registerReceiver(
+            activity: FragmentActivity, receiver: PostsFragmentBroadcastReceiver? = null
+        ): PostsFragmentBroadcastReceiver {
+            val receiver = receiver ?: PostsFragmentBroadcastReceiver()
+            val filter = IntentFilter(START_NEW_SEARCH)
+            activity.registerReceiver(receiver, filter)
+            return receiver
+        }
+
     }
 }
 
