62da72099d9bb254c1fc6648f242d1198318be09
Makentoshe <mkliverout@gmail.com>
2019-06-15 15:03:28 +0300

    [refactor] Update PostsContentFragment

--- boorupostview/src/main/java/com/makentoshe/boorupostview/fragment/PostsContentFragment.kt
+++ boorupostview/src/main/java/com/makentoshe/boorupostview/fragment/PostsContentFragment.kt
@@ -1,5 +1,6 @@
 package com.makentoshe.boorupostview.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,28 +13,19 @@ import com.makentoshe.api.NetworkExecutorBuilder
 import com.makentoshe.api.TagRepository
 import com.makentoshe.boorulibrary.booru.entity.Booru
 import com.makentoshe.boorulibrary.entitiy.Tag
-import com.makentoshe.boorupostview.presenter.PostsContentFragmentPresenter
-import com.makentoshe.boorupostview.presenter.PostsContentFragmentRxPresenterImpl
+import com.makentoshe.boorupostview.PostsFragmentBroadcastReceiver
 import com.makentoshe.boorupostview.R
+import com.makentoshe.boorupostview.presenter.PostsContentFragmentRxPresenter
 import com.makentoshe.boorupostview.view.PostsContentFragmentUi
-import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import io.reactivex.disposables.CompositeDisposable
 import org.jetbrains.anko.AnkoContext
 import java.io.Serializable
 
+/**
+ * Fragment for the content view in panel layout.
+ * It contains selected tags, search text view and start search button.
+ */
 class PostsContentFragment : Fragment() {
-
-    private val panelLayout: SlidingUpPanelLayout by lazy {
-        val panelLayoutId = R.id.slidingPanel
-        return@lazy requireActivity().findViewById<SlidingUpPanelLayout>(panelLayoutId)
-    }
-
-    private val presenter: PostsContentFragmentPresenter by lazy {
-        val networkExecutor = NetworkExecutorBuilder().build()
-        val autocompleteRepository = AutocompleteRepository(booru, networkExecutor)
-        val tagRepository = TagRepository(booru)
-        PostsContentFragmentRxPresenterImpl(disposables, autocompleteRepository, tagRepository, panelLayout, tags)
-    }
 
     private var booru: Booru
         set(value) = (arguments ?: Bundle().also { arguments = it }).putSerializable(BOORU, value)
@@ -43,28 +35,50 @@ class PostsContentFragment : Fragment() {
         set(value) = (arguments ?: Bundle().also { arguments = it }).putSerializable(TAGS, value as Serializable)
         get() = arguments!!.get(TAGS) as Set<Tag>
 
+    /** Broadcast receiver for receiving a new search events from another fragment */
+    private val broadcastReceiver = PostsFragmentBroadcastReceiver()
+
+    /** Contains a disposable which will be released on destroy lifecycle event */
     private val disposables = CompositeDisposable()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    /** Register receiver */
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        PostsFragmentBroadcastReceiver.registerReceiver(requireActivity(), broadcastReceiver)
+        // save tags to the args on new search event
+        broadcastReceiver.onNewSearchStarted { tags = it }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return PostsContentFragmentUi().createView(AnkoContext.create(requireContext()))
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // create presenter
+        val networkExecutor = NetworkExecutorBuilder.buildStreamGet()
+        val autocompleteRepository = AutocompleteRepository(booru, networkExecutor)
+        val tagRepository = TagRepository(booru)
+        val presenter = PostsContentFragmentRxPresenter(
+            disposables, autocompleteRepository, tagRepository, requireContext(), tags
+        )
+        // bind search button
         val searchbutton = view.findViewById<View>(R.id.search_button)
         presenter.bindSearchButton(searchbutton)
-
+        // bind search edit text
         val edittext = view.findViewById<AutoCompleteTextView>(R.id.search_edit_text)
         presenter.bindEditText(edittext)
-
+        // bind chip group
         val chipGroup = view.findViewById<ChipGroup>(R.id.search_chip_group)
         presenter.bindChipGroup(chipGroup)
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        val tags = HashSet<Tag>().apply { addAll(presenter.tags) }
-        this.tags = tags
+    /** Unregister receiver */
+    override fun onDetach() {
+        super.onDetach()
+        requireActivity().unregisterReceiver(broadcastReceiver)
     }
 
+    /** Clear disposables */
     override fun onDestroy() {
         super.onDestroy()
         disposables.clear()
