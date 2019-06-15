97dc924208a7d97be5d68e8f006fba26e6aaa484
Makentoshe <mkliverout@gmail.com>
2019-06-15 14:43:37 +0300

    [refactor] Update docs. Remove value

--- boorupostview/src/main/java/com/makentoshe/boorupostview/presenter/PostsContentFragmentPresenter.kt
+++ boorupostview/src/main/java/com/makentoshe/boorupostview/presenter/PostsContentFragmentPresenter.kt
@@ -6,16 +6,24 @@ import com.google.android.material.chip.ChipGroup
 import com.makentoshe.boorulibrary.entitiy.Tag
 import java.io.Serializable
 
+/**
+ * Presenter component for search layout
+ */
 interface PostsContentFragmentPresenter : Serializable {
 
-    /**
-     * Contains a set of the selected tags.
-     */
-    val tags: Set<Tag>
-
+    /** Bind a [View] as a search button. Should start search on click */
     fun bindSearchButton(view: View)
 
+    /**
+     * Bind an [AutoCompleteTextView] as a search edit text.
+     * Should add a tag to the [ChipGroup] on space symbol input.
+     * Should autocomplete tags by title and add a tag to the [ChipGroup] on dropdown list element clicked.
+     * Should add a tag to the [ChipGroup] and start a search on the ime action.
+     */
     fun bindEditText(view: AutoCompleteTextView)
 
+    /**
+     * Bind a [ChipGroup]. Contains a Chips as a Tags.
+     */
     fun bindChipGroup(view: ChipGroup)
 }
