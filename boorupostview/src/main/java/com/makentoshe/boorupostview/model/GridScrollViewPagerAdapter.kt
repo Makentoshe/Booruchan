aa66f407c52f24566d8081de3123536245c62084
Makentoshe <mkliverout@gmail.com>
2019-06-15 13:07:07 +0300

    [feature] Add a viewpager adapter for displaying a posts in a pages

---
+++ boorupostview/src/main/java/com/makentoshe/boorupostview/model/GridScrollViewPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.makentoshe.boorupostview.model
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.makentoshe.boorulibrary.booru.entity.Booru
+import com.makentoshe.boorulibrary.entitiy.Tag
+import com.makentoshe.boorupostview.fragment.GridScrollElementFragment
+
+/**
+ * Adapter for a view pager, creates a fragments contains a grid view for a posts displaying
+ */
+class GridScrollViewPagerAdapter(
+    private val booru: Booru, private val tags: Set<Tag>, fm: FragmentManager
+) : FragmentStatePagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return GridScrollElementFragment.build(booru, tags, position)
+    }
+
+    override fun getCount() = Int.MAX_VALUE
+
+}
