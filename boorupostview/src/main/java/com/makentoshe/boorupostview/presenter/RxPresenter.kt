e1b10d870e5b378fd5344be2bbde51bb95dd29ef
Makentoshe <mkliverout@gmail.com>
2019-06-15 12:44:04 +0300

    [docs] Add description for the RxPresenter

--- boorupostview/src/main/java/com/makentoshe/boorupostview/presenter/RxPresenter.kt
+++ boorupostview/src/main/java/com/makentoshe/boorupostview/presenter/RxPresenter.kt
@@ -2,6 +2,11 @@ package com.makentoshe.boorupostview.presenter
 
 import io.reactivex.disposables.CompositeDisposable
 
+/**
+ * Presenter component which contains a disposable container
+ */
 abstract class RxPresenter {
+
+    /** Container for the disposables */
     abstract val disposables: CompositeDisposable
 }
