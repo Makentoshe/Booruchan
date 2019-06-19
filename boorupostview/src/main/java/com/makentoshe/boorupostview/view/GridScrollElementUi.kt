776d88826649bbb9133469a7c841821ab2ed5c0c
Makentoshe <mkliverout@gmail.com>
2019-06-19 11:59:10 +0300

    [refactor] Update GridView's element ui

--- boorupostview/src/main/java/com/makentoshe/boorupostview/view/GridScrollElementUi.kt
+++ boorupostview/src/main/java/com/makentoshe/boorupostview/view/GridScrollElementUi.kt
@@ -1,9 +1,9 @@
 package com.makentoshe.boorupostview.view
 
 import android.content.Context
+import android.graphics.Color
 import android.view.View
 import org.jetbrains.anko.*
-import org.jetbrains.anko.cardview.v7._CardView
 import org.jetbrains.anko.cardview.v7.cardView
 
 /**
@@ -11,18 +11,33 @@ import org.jetbrains.anko.cardview.v7.cardView
  */
 class GridScrollElementUi : AnkoComponent<Context> {
     override fun createView(ui: AnkoContext<Context>): View = with(ui) {
+        val dip4 = dip(4)
+        val dip8 = dip(8)
+        val dip100 = dip(100)
+
         cardView {
             radius = 0f
-            elevation = dip(4).toFloat()
-            lparams(dip(100), dip(100))
-            createContent()
+            elevation = dip4.toFloat()
+            lparams(dip100, dip100)
+
+            relativeLayout {
+
+                imageView {
+                    id = com.makentoshe.boorupostview.R.id.gridview_element_image
+                    setPadding(dip4, dip4, dip4, dip4)
+                }.lparams(matchParent, matchParent)
+
+                horizontalProgressBar {
+                    id = com.makentoshe.boorupostview.R.id.gridview_element_progress
+                    max = 100
+                    backgroundColor = Color.TRANSPARENT
+                }.lparams(width = matchParent) {
+                    alignParentBottom()
+                    setMargins(dip8, dip8, dip8, 0)
+                }
+
+            }.lparams(matchParent)
+
         }
     }
-
-    private fun _CardView.createContent() = relativeLayout {
-        imageView {
-            id = com.makentoshe.boorupostview.R.id.gridview_element_image
-            setPadding(dip(4), dip(4), dip(4), dip(4))
-        }.lparams(matchParent, matchParent)
-    }.lparams(matchParent, matchParent)
 }
