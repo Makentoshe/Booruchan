a593607e531743bcc4763d82c3d407b9b062b1f7
Makentoshe <mkliverout@gmail.com>
2019-06-19 12:02:12 +0300

    [refactor] Update GridView adapter

--- boorupostview/src/main/java/com/makentoshe/boorupostview/model/GridScrollElementAdapter.kt
+++ boorupostview/src/main/java/com/makentoshe/boorupostview/model/GridScrollElementAdapter.kt
@@ -5,61 +5,33 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
-import com.makentoshe.api.*
+import android.widget.ProgressBar
 import com.makentoshe.boorulibrary.entitiy.Post
-import com.makentoshe.boorulibrary.network.StreamDownloadListener
-import com.makentoshe.boorupostview.PostsFragmentNavigator
-import com.makentoshe.boorupostview.presenter.GridElementRxPresenter
+import com.makentoshe.boorupostview.presenter.GridElementPresenter
 import com.makentoshe.boorupostview.view.GridScrollElementUi
 import io.reactivex.disposables.CompositeDisposable
 import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.find
 
 /**
  * Adapter creates and bindings grid's element views and grid's element controllers.
- *
- * @param posts is a lists with posts.
- * @param repositoryBuilder creates a repository instance for getting a preview images by post
  */
 class GridScrollElementAdapter(
-    private val posts: List<Post>,
-    private val disposables: CompositeDisposable,
-    private val repositoryBuilder: PreviewImageRepository.Builder,
-    private val navigator: PostsFragmentNavigator
+    private val posts: List<Post>, private val disposables: CompositeDisposable
 ) : BaseAdapter() {
-
-    /** Build a preview repository and wrap it by cache decorator */
-    private fun buildPreviewRepository(
-        listener: StreamDownloadListener, context: Context
-    ): Repository<Post, ByteArray> {
-        val networkExecutor = NetworkExecutorBuilder.buildSmartGet(null, listener)
-        val repository = repositoryBuilder.build(networkExecutor)
-        val cache = ImageDiskCache(DiskCache(ImageDiskCache.getPreviewDir(context)))
-        return RepositoryCache(cache, repository)
-    }
 
     /** Create a ui for a grid element and binds a presenter to it */
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val listener = SimpleStreamDownloadListener()
-        val repository = buildPreviewRepository(listener, parent.context)
-        val presenter = GridElementRxPresenter(
-            disposables, repository, getItem(position), listener, navigator
-        )
-        // create or reuse view
+        val presenter = GridElementPresenter(disposables, position)
+        // create or reuse view and binds it to the presenter
         val view = convertView ?: createView(parent.context)
-        presenter.bindRoot(view)
+        // bind a root view
+        presenter.bindView(view)
         // bind a preview image view
-        val imageview = view.find<ImageView>(com.makentoshe.boorupostview.R.id.gridview_element_image)
-        presenter.bindPreview(imageview)
-
-        //        val progressbar = view.find<ProgressBar>(R.id.posts_page_gridview_element_progress)
-        //        val streamProgressBarController = StreamProgressBarController(progressbar)
-
-        //        val typeView = view.find<ImageView>(R.id.posts_page_gridview_element_type)
-        //        ConcreteTypeControllerFactory(typeView).build(getItem(position)).setType()
-
-        //        val controller = controllerBuilder.createController(getItem(position))
-        //        controller.bindControllers(imageReceiveController, streamProgressBarController)
+        val imageview = view.findViewById<ImageView>(com.makentoshe.boorupostview.R.id.gridview_element_image)
+        presenter.bindImageView(imageview)
+        // bind a progress bar
+        val progressbar = view.findViewById<ProgressBar>(com.makentoshe.boorupostview.R.id.gridview_element_progress)
+        presenter.bindProgressbar(progressbar)
 
         return view
     }
