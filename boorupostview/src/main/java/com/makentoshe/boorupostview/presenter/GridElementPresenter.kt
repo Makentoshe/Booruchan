3034ff9d94d4e40930a4e4ec2695c1b9aa29a505
Makentoshe <mkliverout@gmail.com>
2019-06-19 11:56:38 +0300

    [refactor] Remove interface and update GridElementPresenter

--- boorupostview/src/main/java/com/makentoshe/boorupostview/presenter/GridElementPresenter.kt
+++ boorupostview/src/main/java/com/makentoshe/boorupostview/presenter/GridElementPresenter.kt
@@ -2,15 +2,32 @@ package com.makentoshe.boorupostview.presenter
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
+import com.makentoshe.boorupostview.PostSelectBroadcastReceiver
+import io.reactivex.disposables.CompositeDisposable
+import java.io.Serializable
 
 /**
  * Presenter component for a grid element.
+ *
+ * @param position is a post position.
  */
-interface GridElementPresenter {
+class GridElementPresenter(
+    override val disposables: CompositeDisposable, private val position: Int
+): RxPresenter(), Serializable {
 
-    /** Bind a [ImageView] as a preview image. Image receives as a bytearray and decodes to the bitmap*/
-    fun bindPreview(view: ImageView)
+    /** Binds a grid element view */
+    fun bindView(view: View) = view.setOnClickListener {
+        PostSelectBroadcastReceiver.sendBroadcast(view.context, position)
+    }
 
-    /** Bind a root view. Should start new screen on click event */
-    fun bindRoot(view: View)
+    /** Binds a preview image view */
+    fun bindImageView(view: ImageView) {
+
+    }
+
+    /** Binds a progress bar view*/
+    fun bindProgressbar(view: ProgressBar) {
+
+    }
 }
