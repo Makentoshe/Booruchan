ba5efbdd6b764ad0a4627f88fd8f11e98e5de5de
Makentoshe <mkliverout@gmail.com>
2019-06-20 10:28:05 +0300

    [feature] Add Caches for a sample images and full size images. Rework ImageDiskCache class

---
+++ api/src/main/java/com/makentoshe/api/CacheBuilder.kt
@@ -0,0 +1,26 @@
+package com.makentoshe.api
+
+import android.content.Context
+
+/** Class performs a building any types of cache */
+class CacheBuilder(context: Context) {
+
+    /** Path for a preview image cache directory */
+    private val previewDir = ImageDiskCache.getPreviewDir(context)
+
+    /** Path for a sample image cache directory */
+    private val sampleDir = ImageDiskCache.getSampleDir(context)
+
+    /** Path for a full size image cache directory */
+    private val fileDir = ImageDiskCache.getFileDir(context)
+
+    /** Factory method builds and returns an instance of a Cache object for a preview images */
+    fun buildPreviewCache() = ImageDiskCache(previewDir)
+
+    /** Factory method builds and returns an instance of a Cache object for a sample images */
+    fun buildSampleCache() = ImageDiskCache(sampleDir)
+
+    /** Factory method builds and returns an instance of a Cache object for a full size images */
+    fun buildFileCache() = ImageDiskCache(fileDir)
+
+}
--- api/src/main/java/com/makentoshe/api/ImageDiskCache.kt
+++ api/src/main/java/com/makentoshe/api/ImageDiskCache.kt
@@ -1,22 +1,47 @@
 package com.makentoshe.api
 
 import android.content.Context
+import android.util.Log
+import com.makentoshe.api.BuildConfig.DEBUG
 import com.makentoshe.boorulibrary.entitiy.Post
 import java.io.File
 
 /**
  * Class for caching image as a [ByteArray] using a [Post] as a key.
  */
-class ImageDiskCache(private val diskCache: DiskCache) : Cache<Post, ByteArray> {
+class ImageDiskCache(private val directory: File) : Cache<Post, ByteArray> {
 
-    override fun get(key: Post) = diskCache.get(key.id.toString())
+    /** Key, Path */
+    private val hashMap = LinkedHashMap<String, String>()
 
-    override fun add(key: Post, value: ByteArray) = diskCache.add(key.id.toString(), value)
+    init {
+        directory.listFiles().filter { it.isFile }.forEach { hashMap[it.name] = it.path }
+    }
 
-    override fun clear() = diskCache.clear()
+    override fun get(key: Post): ByteArray? {
+        return key.id.toString().let { key -> if (hashMap.containsKey(key)) File(hashMap[key]).readBytes() else null }
+    }
+
+    override fun clear() {
+        hashMap.clear()
+        directory.deleteRecursively()
+    }
+
+    override fun add(key: Post, value: ByteArray) = File(directory, key.id.toString()).let {
+        val isCreated = it.createNewFile()
+        if (DEBUG) Log.i("Caches", "key=$key\nisCreated=$isCreated")
+        it.writeBytes(value)
+        hashMap[it.name] = it.path
+    }
 
     companion object {
         /** Returns a cache directory for the preview images */
         fun getPreviewDir(context: Context): File = context.getDir("Preview", Context.MODE_PRIVATE)
+        /** Returns a cache instance for the preview images */
+        fun getPreviewCache(context: Context): Cache<Post, ByteArray> = ImageDiskCache(getPreviewDir(context))
+        /** Returns a cache directory for the sample images */
+        fun getSampleDir(context: Context): File = context.getDir("Sample", Context.MODE_PRIVATE)
+        /** Returns a cache directory for the full size images */
+        fun getFileDir(context: Context): File = context.getDir("File", Context.MODE_PRIVATE)
     }
 }
