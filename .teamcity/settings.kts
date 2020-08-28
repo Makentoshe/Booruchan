897dc383176c356950022a0a3aa0d48874dda620
Evgeny.Bovykin@jetbrains.com <Evgeny.Bovykin@jetbrains.com@TeamCity>
2020-08-28 13:53:12 +0000

    TeamCity change in 'Booruchan' project: 'Test' build configuration was created

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -25,4 +25,10 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2020.1"
 
 project {
+
+    buildType(Test)
 }
+
+object Test : BuildType({
+    name = "Test"
+})
