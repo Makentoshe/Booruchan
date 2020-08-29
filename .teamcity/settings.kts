63f81d71ad1c09e8308f65da00903c84ca45c9ab
Maksim.Khvostov <maksim.khvostov@jetbrains.com>
2020-08-29 14:11:42 +0300

    ci(internal): Add core and gelbooru modules build steps
    With a russian commentaries in it

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -26,6 +26,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
+
 version = "2020.1"
 
 project {
@@ -35,22 +36,54 @@ project {
 
 object InternalBuild : BuildType({
     name = "Internal"
-    id("internal")
+    id("internal") // не обязательно указывать
+    // чому?
+    // тимсити сам выведет
 
     vcs {
-        add(DslContext.settingsRoot.id!!)
+        root(BooruchanVcsRoot)
     }
 
+    //    короче ты работай, а я посмотрю и подскажу если что
+    //    бля, легко сказать
+    //    Я хуй знает че счас дальше делать
+    //    А что ты хочешь?
+    //            ну пока я буду разбираться с тем, какие шаги мне вообще нужны
+    // счас прикол в том, что билд билдит весь проект без зависимостей, и надо как то это исправить. В смысле, билдить по модулям?
+    // Да, Типо того и подпихивать нужные жарники в нужные модули, типа снежного кома, чтоб это говно собиралось
+    // Отдельный модуль собрать это `grale :module-name:build`. Про jar-ники я уже говорил, скорее всего кеши грэдла тебе похерят эту идею. И нужно тогда явно указывать зависимость
+    // вида implementation("my-module.jar"). Но тогда у тебя проект собрать по простому нельзя будет, только каждый модуль по отдельности
+    // да, это я знаю. Вот я и хочу как-то это обойти
+    // мб я думаю, можно ли добавить для градла аргументы, чтоб он их распарсил и рещил как собирать
+    // (да можно. В команд лайне это выглядит как -Pargument=value. В самом грэдле получаешь через rootProject.findParameter или rootProject.getParameter)
+    // ну да, можно сделать if (partialBuild) implementation("my-module.jar") else implementation(":my-module")
+    //, и сделать через ифы
+    // Понял. Ну и как-то управлять dependencies через этот иф, если а то по дефолту если б то модули
+    // Да, в этом и мысль
+    // Бля, если не знать, как этот коментарий появился, похоже на какую-то шизу
+    // У gradlew есть аргументы? Для степа?
+
     steps {
+//        script {
+//            name = "Clean before install"
+//            executionMode = BuildStep.ExecutionMode.ALWAYS
+//            scriptContent = "ls"
+//        }
         gradle {
-            name = "Default build"
-            tasks = "clean build --info --debug"
+            name = "Core module build"
+            tasks = "clean :booruchan-core:build"
             buildFile = "build.gradle"
+//            gradleArguments
+//            gradleParameters // че-то такое
+            // Ок, пойду с ноута гляну, там не отвалился автокомплит. Обидно что отвалился. Вообще, так можно хакатон проекты пилить :)
+            // Когда следующий будет? Компанейский через год. Вроде в тимсити что-то хотели, но пока не понятно
+            // А то можно реализовать ту доку 13 года по приколу. Как вариант
+            // Ну чоч - пойду пробовать запускать на тс эту штуку
         }
-        script {
-            name = "Clean before install"
-            executionMode = BuildStep.ExecutionMode.ALWAYS
-            scriptContent = "ls"
+        gradle {
+            name = "Gelbooru module build"
+            tasks = "clean :booruchan-gelbooru:build"
+            gradleParams = "-Pargument=value"
         }
     }
 })
