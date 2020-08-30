e697ba2de420ebbd60baada13b0ef2ef633cab74
Makentoshe <mkliverout@gmail.com>
2020-08-30 14:42:13 +0300

    ci(*): Add several configurations

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,10 +1,11 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.PublishMode
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2019_2.version
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -32,8 +33,92 @@ version = "2020.1"
 
 project {
     vcsRoot(BooruchanVcsRoot)
-    buildType(InternalBuild)
+//    buildType(InternalBuild)
+
+    buildType(Core)
+    buildType(Gelbooru)
+    buildType(Danbooru)
 }
+
+object Core : BuildType({
+    name = "Core"
+
+    publishArtifacts = PublishMode.ALWAYS
+    artifactRules = "./booruchan-core/build/libs/* => core"
+
+    steps {
+        gradle {
+            name = "$name module build"
+            tasks = "booruchan-core:build"
+            buildFile = "build.gradle.kts"
+        }
+    }
+
+    vcs {
+        root(BooruchanVcsRoot)
+    }
+
+    // Triggers are used to add builds to the queue either when an event occurs (like a VCS check-in)
+    // or periodically with some configurable interval
+    triggers { vcs { } }
+})
+
+object Gelbooru : BuildType({
+    name = "Gelbooru"
+
+    dependencies {
+        snapshot(Core) {}
+        artifacts(Core) {
+            artifactRules = "core/* => ./booruchan-gelbooru/libs"
+        }
+    }
+
+    steps {
+        gradle {
+            name = "$name module build"
+            tasks = ":booruchan-gelbooru:build"
+            gradleParams = "-Pmodular"
+            buildFile = "build.gradle.kts"
+        }
+    }
+
+    vcs {
+        root(BooruchanVcsRoot)
+    }
+
+    // Triggers are used to add builds to the queue either when an event occurs (like a VCS check-in)
+    // or periodically with some configurable interval
+    triggers { vcs { } }
+})
+
+object Danbooru : BuildType({
+    name = "Danbooru"
+
+    dependencies {
+        snapshot(Core) {}
+        artifacts(Core) {
+            artifactRules = "core/* => ./booruchan-danbooru/libs"
+        }
+    }
+
+    steps {
+        gradle {
+            name = "$name module build"
+            tasks = ":booruchan-danbooru:build"
+            gradleParams = "-Pmodular"
+            buildFile = "build.gradle.kts"
+        }
+    }
+
+    vcs {
+        root(BooruchanVcsRoot)
+    }
+
+    // Triggers are used to add builds to the queue either when an event occurs (like a VCS check-in)
+    // or periodically with some configurable interval
+    triggers { vcs { } }
+
+})
 
 object InternalBuild : BuildType({
     name = "Internal"
@@ -46,9 +131,9 @@ object InternalBuild : BuildType({
         root(BooruchanVcsRoot)
     }
 
-    triggers {
-        vcs { }
-    }
+    // Triggers are used to add builds to the queue either when an event occurs (like a VCS check-in)
+    // or periodically with some configurable interval
+    triggers { vcs { } }
 
     steps {
         val coreJarDirectory = "./booruchan-core/build/libs"
@@ -58,10 +143,6 @@ object InternalBuild : BuildType({
             name = "Core module build"
             tasks = "booruchan-core:build"
             buildFile = "build.gradle.kts"
-        }
-        script {
-            name = "Debug check"
-            scriptContent = "ls -R $coreJarDirectory"
         }
         script {
             name = "Delivery core artifacts to dependent modules"
@@ -83,19 +164,18 @@ object InternalBuild : BuildType({
         }
     }
 
-
-
-    features {
-        pullRequests {
-            vcsRootExtId = "${DslContext.settingsRoot.id}"
-            provider = github {
-                authType = token {
-                    token = "credentialsJSON:48aba3a6-9f18-45d4-9d8d-ec3744e9f450"
-                }
-                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
-            }
-        }
-    }
+//    // A build feature is a piece of functionality that can affect a build process or reporting its results.
+//    features {
+//        pullRequests {
+//            vcsRootExtId = "${DslContext.settingsRoot.id}"
+//            provider = github {
+//                authType = token {
+//                    token = "credentialsJSON:48aba3a6-9f18-45d4-9d8d-ec3744e9f450"
+//                }
+//                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+//            }
+//        }
+//    }
 })
 
 object BooruchanVcsRoot : GitVcsRoot({
