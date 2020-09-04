93b805ac64a2fdb273195b0738a31272d60e6d27
Makentoshe <mkliverout@gmail.com>
2020-09-04 23:36:21 +0300

    ci(danbooru-api-check): Update artifact dependencies

--- .teamcity/build/DanbooruApiCheck.kt
+++ .teamcity/build/DanbooruApiCheck.kt
@@ -5,12 +5,15 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 /**
  * Checks Danbooru's real api to be sure, that is all works properly
  * */
-object DanbooruApiCheck: PipelineBuild("Danbooru api check", {
+object DanbooruApiCheck : PipelineBuild("Danbooru api check", {
 
     dependencies {
-        snapshot(build.Danbooru) {}
-        artifacts(build.Danbooru) {
+        snapshot(Core) {}
+        artifacts(Core) {
             artifactRules = "core/* => ./danbooru/libs"
+        }
+        snapshot(Danbooru) {}
+        artifacts(Danbooru) {
             artifactRules = "danbooru/* => ./danbooru-test-api/libs"
         }
     }
