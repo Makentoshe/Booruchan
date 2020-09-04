7f045d834c8d85ca6cde23f14fca7c756a9baf39
Makentoshe <mkliverout@gmail.com>
2020-09-04 18:13:35 +0300

    ci(danbooru): Fix imports

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,4 @@
-import build.Core
-import build.Danbooru
-import build.Gelbooru
+import build.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
 import vcs.BooruchanVcsRoot
