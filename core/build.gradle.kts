ace0c9668a1f2325085d76f8eb00e74a45e17df4
Makentoshe <mkliverout@gmail.com>
2020-09-11 19:23:53 +0300

    feat(core): Add task for building fatJar

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
+    id("com.github.johnrengelman.shadow") version "6.0.0"
 }
 
 group = "com.makentoshe.booruchan.core"
@@ -7,8 +8,21 @@ version = "1.0"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
+}
+
+// executes "shadowJar" task straight after "build"
+tasks.build {
+    finalizedBy(tasks.shadowJar)
+}
+
+// "shadowJar" task configurations
+tasks.shadowJar {
+    archiveBaseName.set("${project.name}-shadow")
+    archiveClassifier.set("")
+    archiveVersion.set(project.version.toString())
 }
