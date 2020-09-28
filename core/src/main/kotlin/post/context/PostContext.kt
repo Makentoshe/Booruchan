7e3c1f50291d36c9247892d7ecbd60c1666aa9ee
Makentoshe <mkliverout@gmail.com>
2020-09-28 17:08:03 +0300

    feat(core): Add PostsContext

--- core/src/main/kotlin/post/context/PostContext.kt
+++ core/src/main/kotlin/post/context/PostContext.kt
@@ -1,27 +1,10 @@
 package post.context
 
+import context.BooruEntityContext
 import post.deserialize.PostDeserialize
 import post.network.PostRequest
 
 open class PostContext<in Request: PostRequest>(
-    private val network: suspend (Request) -> Result<String>,
-    private val deserialize: (String) -> Result<PostDeserialize<*>>
-) {
-
-    suspend fun get(request: Request): Result<PostDeserialize<*>> {
-        val networkResult = network(request)
-        if (networkResult.isFailure) return Result.failure(
-            networkResult.exceptionOrNull() ?: IllegalStateException("Could not define network exception")
-        )
-
-        val networkSuccess = networkResult.getOrNull() ?: throw IllegalStateException("Could not define network result")
-        val deserializeResult = deserialize(networkSuccess)
-        if (deserializeResult.isFailure) return Result.failure(
-            deserializeResult.exceptionOrNull() ?: IllegalStateException("Could not define deserialize exception")
-        )
-
-        return Result.success(
-            deserializeResult.getOrNull() ?: throw IllegalStateException("Could not define deserialize result")
-        )
-    }
-}
+    network: suspend (Request) -> Result<String>,
+    deserialize: (String) -> Result<PostDeserialize<*>>
+): BooruEntityContext<Request, PostDeserialize<*>>(network, deserialize)
