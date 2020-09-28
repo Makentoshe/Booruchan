268ee3feab0c6b9c856261b739862a0b7f69dc1a
Makentoshe <mkliverout@gmail.com>
2020-09-28 17:09:10 +0300

    test(danbooru): Add tests for the PostContext for the danbooru

--- danbooru-api-check/src/test/kotlin/post/context/XmlDanbooruPostsContextTest.kt
+++ danbooru-api-check/src/test/kotlin/post/context/XmlDanbooruPostsContextTest.kt
@@ -6,14 +6,12 @@ import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.Timeout
-import post.network.DanbooruPostFilter
-import post.network.DanbooruPostNetworkManager
-import post.network.XmlDanbooruPostRequest
-import post.postId
+import post.network.DanbooruPostsFilter
+import post.network.DanbooruPostsNetworkManager
+import post.network.XmlDanbooruPostsRequest
 import java.util.logging.Logger
 
-// todo add request post with id 2
-class XmlDanbooruPostContextTest {
+class XmlDanbooruPostsContextTest {
 
     private val logger = Logger.getLogger(this.javaClass.simpleName)
 
@@ -21,18 +19,17 @@ class XmlDanbooruPostContextTest {
     val globalTimeout: Timeout = Timeout.seconds(30)
 
     @Test
-    fun `should request xml post`() = runBlocking {
-        val context = XmlDanbooruPostContext { DanbooruPostNetworkManager(HttpClient()).getPost(it) }
+    fun `should request xml posts`() = runBlocking {
+        val context = XmlDanbooruPostsContext { DanbooruPostsNetworkManager(HttpClient()).getPosts(it) }
 
-        val request = XmlDanbooruPostRequest(DanbooruPostFilter.ById(postId(1)))
+        val request = XmlDanbooruPostsRequest(DanbooruPostsFilter(count = 5))
         logger.info { "Xml url request: ${request.url}" }
-        assertEquals("https://danbooru.donmai.us/posts/1.xml", request.url)
+        assertEquals("https://danbooru.donmai.us/posts.xml?limit=5", request.url)
 
         val result = context.get(request)
         logger.info { "Result: $result" }
         val successResult = context.get(request).getOrNull()!!
 
-        assertEquals(1, successResult.post.postId)
+        assertEquals(5, successResult.deserializes.size)
     }
-
 }
