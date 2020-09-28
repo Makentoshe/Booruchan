048189e2d330aa0434df0d9bef69cd1facfd0a40
Makentoshe <mkliverout@gmail.com>
2020-09-28 17:08:28 +0300

    feat(danbooru): Implement PostsContext for Json and Xml

--- danbooru/src/main/kotlin/post/context/DanbooruPostContext.kt
+++ danbooru/src/main/kotlin/post/context/DanbooruPostContext.kt
@@ -4,16 +4,22 @@ import post.deserialize.DanbooruPostDeserialize
 import post.deserialize.JsonDanbooruPostDeserializer
 import post.deserialize.XmlDanbooruPostDeserializer
 import post.network.DanbooruPostRequest
+import post.network.JsonDanbooruPostRequest
+import post.network.XmlDanbooruPostRequest
 
-open class DanbooruPostContext(
-    network: suspend (DanbooruPostRequest) -> Result<String>,
+open class DanbooruPostContext<in Request: DanbooruPostRequest>(
+    network: suspend (Request) -> Result<String>,
     deserialize: (String) -> Result<DanbooruPostDeserialize<*>>
-) : PostContext<DanbooruPostRequest>(network, deserialize)
+) : PostContext<Request>(network, deserialize)
 
 open class JsonDanbooruPostContext(
-    network: suspend (DanbooruPostRequest) -> Result<String>
-) : DanbooruPostContext(network, { json -> JsonDanbooruPostDeserializer().deserializePost(json) })
+    network: suspend (JsonDanbooruPostRequest) -> Result<String>
+) : DanbooruPostContext<JsonDanbooruPostRequest>(
+    network, { json -> JsonDanbooruPostDeserializer().deserializePost(json) }
+)
 
 open class XmlDanbooruPostContext(
-    network: suspend (DanbooruPostRequest) -> Result<String>
-) : DanbooruPostContext(network, { xml -> XmlDanbooruPostDeserializer().deserializePost(xml) })
+    network: suspend (XmlDanbooruPostRequest) -> Result<String>
+) : DanbooruPostContext<XmlDanbooruPostRequest>(
+    network, { xml -> XmlDanbooruPostDeserializer().deserializePost(xml) }
+)
--- danbooru/src/main/kotlin/post/deserialize/DanbooruPostsDeserialize.kt
+++ danbooru/src/main/kotlin/post/deserialize/DanbooruPostsDeserialize.kt
@@ -4,9 +4,8 @@ import deserialize.EntityDeserializeException
 import post.DanbooruPost
 
 data class DanbooruPostsDeserialize<out Post : DanbooruPost>(
-    val deserializes: List<Result<DanbooruPostDeserialize<Post>>>
-) {
-    val failures = deserializes.mapNotNull { it.exceptionOrNull() as? EntityDeserializeException }
-
-    val posts = deserializes.mapNotNull { it.getOrNull()?.post }
+    override val deserializes: List<Result<DanbooruPostDeserialize<Post>>>
+) : PostsDeserialize<Post> {
+    override val failures = deserializes.mapNotNull { it.exceptionOrNull() as? EntityDeserializeException }
+    override val posts = deserializes.mapNotNull { it.getOrNull()?.post }
 }
