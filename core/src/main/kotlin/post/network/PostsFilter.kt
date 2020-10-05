d167a74220a581b4cc37c43dd4cc7f11c1436a80
Makentoshe <mkliverout@gmail.com>
2020-10-05 15:11:33 +0300

    feat(danbooru): Update Filter interfaces

--- core/src/main/kotlin/post/network/PostsFilter.kt
+++ core/src/main/kotlin/post/network/PostsFilter.kt
@@ -2,4 +2,14 @@ package post.network
 
 import network.Filter
 
-interface PostsFilter : Filter
+abstract class PostsFilter(protected val params: Map<String, Any>) : Filter {
+
+    override fun toUrl(): String {
+        if (params.isEmpty()) return ""
+        return params.entries.mapIndexed { index, entry ->
+            val builder = StringBuilder()
+            if (index == 0) builder.append("?") else builder.append("&")
+            builder.append(entry.key).append("=").append(entry.value)
+        }.joinToString { it.toString() }
+    }
+}
