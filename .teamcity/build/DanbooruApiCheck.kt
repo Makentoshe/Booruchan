174fdee139f6826316ca3460753f661c194f0daa
Makentoshe <mkliverout@gmail.com>
2020-10-06 19:05:53 +0300

    ci(danbooru): Update build artifacts

--- .teamcity/build/DanbooruApiCheck.kt
+++ .teamcity/build/DanbooruApiCheck.kt
@@ -1,11 +1,18 @@
 package build
 
+import jetbrains.buildServer.configs.kotlin.v2019_2.PublishMode
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 
 /**
  * Checks Danbooru's real api to be sure, that is all works properly
  * */
 object DanbooruApiCheck : PipelineBuildDaily("Danbooru api check", 0, 0, {
+
+    publishArtifacts = PublishMode.SUCCESSFUL
+    artifactRules = """
+        ./danbooru/build/libs/danbooru-shadow-* => danbooru
+        ./danbooru/build/reports/jacoco/test/html-zip/* => jacocoHtmlReport
+    """.trimIndent()
 
     dependencies {
         snapshot(Core) {}
