e03a51939ac5c7214ec2ec5693e00f7e40ba2bb6
Makentoshe <mkliverout@gmail.com>
2020-10-07 18:15:34 +0300

    ci(danbooru): Update artifact paths

--- .teamcity/build/DanbooruComposite.kt
+++ .teamcity/build/DanbooruComposite.kt
@@ -1,8 +1,14 @@
 package build
 
-object DanbooruComposite : PipelineBuild("Danbooru Composite", {
+import jetbrains.buildServer.configs.kotlin.v2019_2.PublishMode
 
+object DanbooruComposite : PipelineBuild("Danbooru Composite", {
     this.type = Type.COMPOSITE
+
+    publishArtifacts = PublishMode.NORMALLY_FINISHED
+    artifactRules = """
+        ./danbooru-api-check/build/reports/jacoco/test/html-zip/* => jacocoHtmlReport
+    """.trimIndent()
 
     vcs {
         showDependenciesChanges = true
