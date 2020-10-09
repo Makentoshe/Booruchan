5ecd6e9745731e4146e850d308e1bc0da09dd316
Makentoshe <mkliverout@gmail.com>
2020-10-09 19:53:53 +0300

    build(*): Add task that aggregates jacoco html reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    jacoco // enable JaCoco plugin
 }
 
 group = "com.makentoshe.booruchan"
@@ -20,3 +21,32 @@ compileKotlin.kotlinOptions.jvmTarget = "1.8"
 
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions.jvmTarget = "1.8"
+
+// Task allows to run and aggregate coverage reports for whole project
+// NOTE: the `JacocoReport` tasks do *not* depend on the `test` task by default. Meaning you have to ensure
+// that `test` (or other tasks generating code coverage) run before generating the report.
+tasks.register<JacocoReport>("testJacocoCoverageReport") {
+    // If a subproject applies the 'jacoco' plugin, add the result it to the report
+    subprojects {
+        val subproject = this
+        subproject.plugins.withType<JacocoPlugin>().configureEach {
+            subproject.tasks.matching { it.extensions.findByType<JacocoTaskExtension>() != null }.configureEach {
+                val testTask = this
+                sourceSets(subproject.sourceSets.main.get())
+                executionData(this)
+            }
+
+            // To automatically run `test` every time `./gradlew codeCoverageReport` is called,
+            // you may want to set up a task dependency between them as shown below.
+            // Note that this requires the `test` tasks to be resolved eagerly (see `forEach`) which
+            // may have a negative effect on the configuration time of your build.
+            subproject.tasks.matching { it.extensions.findByType<JacocoTaskExtension>() != null }.forEach {
+                rootProject.tasks["testJacocoCoverageReport"].dependsOn(it)
+            }
+        }
+    }
+
+    reports {
+        html.isEnabled = true
+    }
+}
