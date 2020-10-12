7d53f067e6b1c691b98089da4bfefebacc7a0966
Makentoshe <mkliverout@gmail.com>
2020-10-12 16:12:02 +0300

    ci(danbooru): Update build dependencies

--- .teamcity/build/JacocoCoverageReport.kt
+++ .teamcity/build/JacocoCoverageReport.kt
@@ -7,7 +7,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
  * Build aggregates jacoco coverage reports from each module
  * that uses jacoco plugin and publish as build report for whole project
  *
- * The main idea that the each module runs own jacoco tests
+ * The main idea that the each build runs own jacoco tests for own module
  * and coverage tasks. After all this reports aggregates in one
  * for better coverage understanding.
  */
@@ -21,10 +21,7 @@ object JacocoCoverageReport: PipelineBuildVcs("Test coverage report", {
     // Build depends on all other builds and collects their reports to merge
     dependencies {
         snapshot(Core) {}
-
         snapshot(Danbooru) {}
-        snapshot(DanbooruNetworkCheck) {}
-
         snapshot(Gelbooru) {}
     }
 
