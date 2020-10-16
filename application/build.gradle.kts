5aaa793e36a4fde5f8745428875a40f0b3fc058e
Makentoshe <mkliverout@gmail.com>
2020-10-16 21:09:27 +0300

    feat(application): Add application module
    This is a main module that contains core, features and release applications

---
+++ application/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+}
+
+group = "com.makentoshe.booruchan.application"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,13 @@
 rootProject.name = "Booruchan"
-include("core")
-include("gelbooru")
-include("danbooru")
 
+include("core")
+
+include("danbooru")
 include("danbooru:network-check")
 findProject(":danbooru:network-check")?.name = "network-check"
+
+include("gelbooru")
 include("gelbooru:network-check")
 findProject(":gelbooru:network-check")?.name = "network-check"
+
+include("application")
