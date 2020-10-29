1433673bca8c2c76eacca30644a28304c5903575
Makentoshe <mkliverout@gmail.com>
2020-10-29 19:29:11 +0300

    feat(core): Add maven-publish plugin

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
-    `java-library`
+    `maven-publish` // for publishing artifacts
 }
 
-group = "com.makentoshe.booruchan.core"
+group = "com.makentoshe.booruchan"
 version = "1.0"
 
 repositories {
@@ -22,3 +22,15 @@ compileKotlin.kotlinOptions.freeCompilerArgs = listOf("-Xallow-result-return-typ
 // Allows to use kotlin.Result type as a return
 val compileTestKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
 compileTestKotlin.kotlinOptions.freeCompilerArgs = listOf("-Xallow-result-return-type")
+
+// configure publish
+publishing {
+    publications {
+        // default jar publication info with transitive dependencies
+        create<MavenPublication>(project.name) { from(components["java"]) }
+    }
+    repositories {
+        // local repository
+        maven { url = uri("file://${rootProject.buildDir}/repository") }
+    }
+}
