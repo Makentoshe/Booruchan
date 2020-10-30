d3e3204b77816f815b6c73f5c06e46dd7cbac0a3
Makentoshe <mkliverout@gmail.com>
2020-10-30 16:13:30 +0300

    feat(gelbooru): Add additional params for the type-safe posts filter constructor

--- gelbooru/src/main/kotlin/com/makentoshe/booruchan/gelbooru/post/network/GelbooruPostsFilter.kt
+++ gelbooru/src/main/kotlin/com/makentoshe/booruchan/gelbooru/post/network/GelbooruPostsFilter.kt
@@ -1,19 +1,26 @@
 package com.makentoshe.booruchan.gelbooru.post.network
 
+import com.makentoshe.booruchan.core.post.Tags
 import com.makentoshe.booruchan.core.post.network.PostsFilter
+import com.makentoshe.booruchan.core.post.string
 
 class GelbooruPostsFilter(params: Map<String, Any>) : PostsFilter(params) {
 
-    constructor(count: Int? = null) : this(buildMap(count))
+    /** Most preferred type-safety constructor */
+    constructor(count: Int? = null, page: Int? = null, tags: Tags = Tags.EMPTY) : this(buildMap(count, page, tags))
 
     override val firstChar: String = "&"
 
     companion object {
         private const val COUNT = "limit"
+        private const val PAGE = "pid"
+        private const val TAGS = "tags"
 
-        private fun buildMap(count: Int?): Map<String, Any> {
+        private fun buildMap(count: Int?, page: Int?, tags: Tags): Map<String, Any> {
             val params = HashMap<String, Any>()
             if (count != null) params[COUNT] = count
+            if (page != null) params[PAGE] = page
+            if (tags.tags.isNotEmpty()) params[TAGS] = tags.string
             return params
         }
     }
