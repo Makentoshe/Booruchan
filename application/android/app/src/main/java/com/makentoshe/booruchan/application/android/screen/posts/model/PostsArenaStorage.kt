cf2c3516d248795e2f0849e6ab5e6a8e5ec64323
Makentoshe <mkliverout@gmail.com>
2020-11-06 20:44:01 +0300

    fix(android): Fix PostsArenaStorage behavior

--- application/android/app/src/main/java/com/makentoshe/booruchan/application/android/screen/posts/model/PostsArenaStorage.kt
+++ application/android/app/src/main/java/com/makentoshe/booruchan/application/android/screen/posts/model/PostsArenaStorage.kt
@@ -3,6 +3,7 @@ package com.makentoshe.booruchan.application.android.screen.posts.model
 import com.makentoshe.booruchan.application.android.BooruchanDatabase
 import com.makentoshe.booruchan.application.android.PostsDeserializeWrapper
 import com.makentoshe.booruchan.application.core.ArenaStorage
+import com.makentoshe.booruchan.application.core.ArenaStorageException
 import com.makentoshe.booruchan.core.post.Post
 import com.makentoshe.booruchan.core.post.context.PostsContext
 import com.makentoshe.booruchan.core.post.deserialize.PostsDeserialize
@@ -15,17 +16,11 @@ class PostsArenaStorage(
 
     override fun fetch(key: PostsFilter): Result<PostsDeserialize<Post>> {
         val postsDeserializeWrapper = database.postsDao().getByFilterUrl(key.toUrl())
-        return postsContext.deserialize.invoke(postsDeserializeWrapper.rawValue).apply { println(this) }
+            ?: return Result.failure(ArenaStorageException("Could not receive record by key: ${key.toUrl()}"))
+        return postsContext.deserialize.invoke(postsDeserializeWrapper.rawValue)
     }
 
-    /** Save result to the storage */
-    override fun carry(key: PostsFilter, value: Result<PostsDeserialize<Post>>) {
-        database.postsDao().clear()
-        println(database.postsDao().getAll())
-
-        val postsDeserialize = value.getOrNull() ?: return println(value)
-        database.postsDao().insert(PostsDeserializeWrapper(key.toUrl(), postsDeserialize.rawValue))
-
-        database.postsDao().getAll().forEach(::println)
+    override fun carry(key: PostsFilter, value: PostsDeserialize<Post>) {
+        database.postsDao().insert(PostsDeserializeWrapper(key.toUrl(), value.rawValue))
     }
 }
