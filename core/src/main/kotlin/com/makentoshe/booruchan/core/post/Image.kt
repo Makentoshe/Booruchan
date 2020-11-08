bb427c719ac341686d047396494d7899c050e33d
Makentoshe <mkliverout@gmail.com>
2020-11-08 14:45:28 +0300

    feat(core): Add "name" parameter for Image object

--- core/src/main/kotlin/com/makentoshe/booruchan/core/post/Image.kt
+++ core/src/main/kotlin/com/makentoshe/booruchan/core/post/Image.kt
@@ -2,43 +2,62 @@ package com.makentoshe.booruchan.core.post
 
 import java.io.File
 
-/** Base image interface provides [url], it's [width] and [height], and [extension] */
+/** Base image interface provides [url], it's [width] and [height], [name] and [extension] */
 interface Image {
     val extension: String
     val url: String
     val height: Int?
     val width: Int?
+    val name: String
 }
 
 /** Typing for [Image] class for full sized images */
 interface FullImage : Image
 
-fun fullImage(url: String, height: Int? = null, width: Int? = null, extension: String = File(url).extension) =
-    object : FullImage {
-        override val url = url
-        override val height = height
-        override val width = width
-        override val extension = extension
-    }
+fun fullImage(
+    url: String,
+    height: Int? = null,
+    width: Int? = null,
+    extension: String = File(url).extension,
+    name: String = File(url).name
+) = object : FullImage {
+    override val url = url
+    override val height = height
+    override val width = width
+    override val extension = extension
+    override val name = name
+}
 
 /** Typing for [Image] class for preview images */
 interface PreviewImage : Image
 
-fun previewImage(url: String, height: Int? = null, width: Int? = null, extension: String = File(url).extension) =
-    object : PreviewImage {
-        override val url = url
-        override val height = height
-        override val width = width
-        override val extension = extension
-    }
+fun previewImage(
+    url: String,
+    height: Int? = null,
+    width: Int? = null,
+    extension: String = File(url).extension,
+    name: String = File(url).name
+) = object : PreviewImage {
+    override val url = url
+    override val height = height
+    override val width = width
+    override val extension = extension
+    override val name = name
+}
 
 /** Typing for [Image] class for images with sample size*/
 interface SampleImage : Image
 
-fun sampleImage(url: String, height: Int? = null, width: Int? = null, extension: String = File(url).extension) =
-    object : SampleImage {
-        override val url = url
-        override val height = height
-        override val width = width
-        override val extension = extension
-    }
+fun sampleImage(
+    url: String,
+    height: Int? = null,
+    width: Int? = null,
+    extension: String = File(url).extension,
+    name: String = File(url).name
+) = object : SampleImage {
+    override val url = url
+    override val height = height
+    override val width = width
+    override val extension = extension
+    override val name = name
+}
