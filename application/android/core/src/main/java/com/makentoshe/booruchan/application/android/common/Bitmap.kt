f847bc6a268c9001c82237d0ce7ee71911a08a6f
Makentoshe <mkliverout@gmail.com>
2020-11-25 01:50:45 +0300

    feat(android): Update extension function for decoding ByteArray to Bitmap instance

--- application/android/core/src/main/java/com/makentoshe/booruchan/application/android/common/Bitmap.kt
+++ application/android/core/src/main/java/com/makentoshe/booruchan/application/android/common/Bitmap.kt
@@ -2,7 +2,19 @@ package com.makentoshe.booruchan.application.android.common
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
 import java.io.ByteArrayInputStream
+import java.nio.ByteBuffer
 
-/** Method for decoding [ByteArray] to android [Bitmap]. */
-fun ByteArray.toBitmap(): Bitmap? = BitmapFactory.decodeStream(ByteArrayInputStream(this))
+/**
+ * Method for decoding [ByteArray] to android [Bitmap].
+ *
+ * We using ImageDecoder for 29 and higher api and default BitmapFactory otherwise.
+ */
+fun ByteArray.toBitmap(): Bitmap? {
+    return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+        ImageDecoder.decodeBitmap(ImageDecoder.createSource(ByteBuffer.wrap(this)))
+    } else {
+        BitmapFactory.decodeByteArray(this, 0, size)
+    }
+}
