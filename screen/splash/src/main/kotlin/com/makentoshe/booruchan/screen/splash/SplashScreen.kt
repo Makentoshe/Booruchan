d7fc7f719a688a71706b846f0f3c6b959054456e
Maxim_Khvostov <mkliverout@gmail.com>
2023-04-18 16:03:13 +0300

    feature: add basic button to SplashScreen

--- screen/splash/src/main/kotlin/com/makentoshe/booruchan/screen/splash/SplashScreen.kt
+++ screen/splash/src/main/kotlin/com/makentoshe/booruchan/screen/splash/SplashScreen.kt
@@ -1,12 +1,27 @@
 package com.makentoshe.booruchan.screen.splash
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
-fun SplashScreen() {
-    Column {
+fun SplashScreen(
+
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
         Text("SplashScreen")
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Splash screen")
+        }
     }
 }
