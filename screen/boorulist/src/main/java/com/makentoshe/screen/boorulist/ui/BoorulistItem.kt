1a57dcb32200233e642998076dfab607d08ecdbd
Maxim_Khvostov <mkliverout@gmail.com>
2023-04-25 18:08:48 +0300

    feature: improve booru item url state

--- screen/boorulist/src/main/java/com/makentoshe/screen/boorulist/ui/BoorulistItem.kt
+++ screen/boorulist/src/main/java/com/makentoshe/screen/boorulist/ui/BoorulistItem.kt
@@ -12,12 +12,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.makentoshe.library.uikit.foundation.IndeterminateProgressBar
 import com.makentoshe.library.uikit.foundation.PrimaryText
 import com.makentoshe.library.uikit.foundation.SecondaryText
 import com.makentoshe.library.uikit.theme.BooruchanTheme
 import com.makentoshe.library.uikit.theme.error
+import com.makentoshe.booruchan.library.resources.R
 import com.makentoshe.screen.boorulist.viewmodel.BooruItemHealthState
 import com.makentoshe.screen.boorulist.viewmodel.BooruItemState
 import com.makentoshe.screen.boorulist.viewmodel.BoorulistEvent
@@ -32,29 +34,32 @@ internal fun BoorulistItem(
     },
     horizontalArrangement = Arrangement.SpaceBetween,
 ) {
-    // Booru title and url
-    Column(
-        modifier = Modifier.padding(start = 16.dp, top = 16.dp, bottom = 16.dp)
-    ) {
-        PrimaryText(text = booruItemState.title)
+    BoorulistItemContent(booruItemState = booruItemState)
 
-        if (booruItemState.health is BooruItemHealthState.Error) {
-            SecondaryText(
-                text = booruItemState.url,
-                color = BooruchanTheme.colors.error,
-            )
-        } else {
-            SecondaryText(
-                text = booruItemState.url,
-                color = BooruchanTheme.colors.dimmed,
-            )
-        }
+    if (booruItemState.health == BooruItemHealthState.Loading) {
+        BoorulistItemHealthProgress()
     }
+}
 
-    // Progress bar at the end
+@Composable
+private fun BoorulistItemContent(booruItemState: BooruItemState) = Column(
+    modifier = Modifier.padding(start = 16.dp, top = 16.dp, bottom = 16.dp)
+) {
+    PrimaryText(text = booruItemState.title)
+
     when (booruItemState.health) {
-        BooruItemHealthState.Loading -> BoorulistItemHealthProgress()
-        else -> Unit
+        BooruItemHealthState.Error -> {
+            val text = stringResource(id = R.string.boorulist_booruitem_error, booruItemState.url)
+            SecondaryText(text = text, color = BooruchanTheme.colors.error)
+        }
+        BooruItemHealthState.Loading -> {
+            val text = stringResource(id = R.string.boorulist_booruitem_loading, booruItemState.url)
+            SecondaryText(text = text, color = BooruchanTheme.colors.opaque)
+        }
+        BooruItemHealthState.Ok -> {
+            val text = stringResource(id = R.string.boorulist_booruitem_ok, booruItemState.url)
+            SecondaryText(text = text, color = BooruchanTheme.colors.dimmed)
+        }
     }
 }
 
