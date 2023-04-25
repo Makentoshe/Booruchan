ccefa567fcc1572da6a282e4e16043299e0c9bb2
Maxim_Khvostov <mkliverout@gmail.com>
2023-04-25 18:09:12 +0300

    feature: add networking permission and add network logging for http client

--- app/src/main/java/com/makentoshe/booruchan/di/ApplicationModule.kt
+++ app/src/main/java/com/makentoshe/booruchan/di/ApplicationModule.kt
@@ -1,5 +1,7 @@
 package com.makentoshe.booruchan.di
 
+import com.makentoshe.booruchan.library.logging.LogFingerprint
+import com.makentoshe.booruchan.library.logging.logInfo
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -7,6 +9,7 @@ import dagger.hilt.components.SingletonComponent
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.*
+import io.ktor.client.plugins.logging.*
 import javax.inject.Singleton
 
 @Module
@@ -18,6 +21,15 @@ object ApplicationModule {
     fun providesHttpClient(): HttpClient = HttpClient(CIO) {
         install(HttpRedirect) {
             checkHttpMethod = false
+        }
+
+        install(Logging) {
+            level = LogLevel.ALL
+            logger = object: Logger {
+                override fun log(message: String) {
+                    logInfo("httpclient", LogFingerprint.Network, message)
+                }
+            }
         }
     }
 
