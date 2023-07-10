5aca021c96216962858d2fb5b4ba8b7bdd1c1d54
Maxim_Khvostov <mkliverout@gmail.com>
2023-07-10 12:50:08 +0300

    feature: remove old autocomplete modules

--- feature/booru-autocomplete-domain/src/main/java/com/makentoshe/booruchan/feature/autocomplete/domain/entity/NetworkSafebooruSearchTag.kt
+++
@@ -1,7 +0,0 @@
-package com.makentoshe.booruchan.feature.autocomplete.domain.entity
-
-@kotlinx.serialization.Serializable
-data class NetworkSafebooruSearchTag(
-    override val value: String,
-    override val label: String,
-): NetworkSearchTag
