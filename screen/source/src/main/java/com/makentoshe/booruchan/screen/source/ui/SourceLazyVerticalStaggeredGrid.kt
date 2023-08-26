4ca499c3e02684ea942a456f32f8d5877823dcae
Maxim_Khvostov <mkliverout@gmail.com>
2023-08-26 15:51:37 +0300

    feature: implement footer for lazy staggered grid

--- screen/source/src/main/java/com/makentoshe/booruchan/screen/source/ui/SourceLazyVerticalStaggeredGrid.kt
+++ screen/source/src/main/java/com/makentoshe/booruchan/screen/source/ui/SourceLazyVerticalStaggeredGrid.kt
@@ -1,27 +1,34 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.makentoshe.booruchan.screen.source.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridScope
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
-import coil.compose.AsyncImage
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import coil.size.Size
@@ -29,9 +36,12 @@ import com.makentoshe.booruchan.screen.source.entity.PreviewPostUiState
 import com.makentoshe.booruchan.screen.source.viewmodel.ContentState
 import com.makentoshe.booruchan.screen.source.viewmodel.SourceScreenState
 import com.makentoshe.library.uikit.foundation.IndeterminateProgressBar
+import com.makentoshe.library.uikit.foundation.PrimaryTextBold
+import com.makentoshe.library.uikit.foundation.SecondaryText
+
+private const val SourceLazyVerticalStaggeredGridFooterKey = "SourceStaggeredGridFooterKey"
 
 @Composable
-@OptIn(ExperimentalFoundationApi::class)
 internal fun SourceLazyVerticalStaggeredGrid(
     screenState: SourceScreenState,
     contentState: ContentState.Success,
@@ -48,6 +58,12 @@ internal fun SourceLazyVerticalStaggeredGrid(
         items(count = previewPostItems.itemCount) { index ->
             val item = previewPostItems[index]
             if (item != null) SourcePreviewPostUi(item = item)
+        }
+
+        when (previewPostItems.loadState.append) {
+            is LoadState.Loading -> sourceFooterLoading()
+            is LoadState.Error -> sourceFooterError()
+            else -> Unit
         }
     }
 }
@@ -75,4 +91,33 @@ private fun SourcePreviewPostUi(
             IndeterminateProgressBar(modifier = Modifier.wrapContentSize())
         }
     )
+}
+
+private fun LazyStaggeredGridScope.sourceFooterLoading() = item(
+    key = SourceLazyVerticalStaggeredGridFooterKey,
+    span = StaggeredGridItemSpan.FullLine,
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth().height(128.dp).padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        content = { IndeterminateProgressBar() }
+    )
+}
+
+private fun LazyStaggeredGridScope.sourceFooterError() = item(
+    key = SourceLazyVerticalStaggeredGridFooterKey,
+    span = StaggeredGridItemSpan.FullLine,
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth().height(128.dp).padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        PrimaryTextBold(text = "Title", textAlign = TextAlign.Start)
+        SecondaryText(text = "Description", textAlign = TextAlign.Center)
+        TextButton(onClick = {}) {
+            PrimaryTextBold(text = "Button")
+        }
+    }
 }
--- screen/source/src/main/java/com/makentoshe/booruchan/screen/source/ui/SourceScreenContent.kt
+++ screen/source/src/main/java/com/makentoshe/booruchan/screen/source/ui/SourceScreenContent.kt
@@ -1,39 +1,66 @@
 package com.makentoshe.booruchan.screen.source.ui
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.makentoshe.booruchan.screen.source.viewmodel.ContentState
 import com.makentoshe.booruchan.screen.source.viewmodel.SourceScreenEvent
 import com.makentoshe.booruchan.screen.source.viewmodel.SourceScreenState
+import com.makentoshe.library.uikit.foundation.IndeterminateProgressBar
+import com.makentoshe.library.uikit.foundation.SecondaryText
+import com.makentoshe.library.uikit.theme.BooruchanTheme
 
 
 @Composable
 internal fun SourceScreenContent(
     screenState: SourceScreenState,
     screenEvent: (SourceScreenEvent) -> Unit,
-) {
-    when (val contentState = screenState.contentState) {
-        ContentState.Loading -> {
-            Text("Loading")
-        }
-        is ContentState.Failure -> {
-            Text("Failure: ${contentState.string}")
-        }
-        is ContentState.Success -> SourceScreenUiSuccess(
-            screenState = screenState,
-            contentState = contentState,
-        )
-    }
+) = when (val contentState = screenState.contentState) {
+    is ContentState.Loading -> SourceScreenContentLoading()
+    is ContentState.Failure -> SourceScreenContentFailure(message = contentState.string)
+    is ContentState.Success -> SourceScreenContentSuccess(screenState = screenState, contentState = contentState)
 }
 
 @Composable
-private fun SourceScreenUiSuccess(
+private fun SourceScreenContentLoading() = Box(
+    modifier = Modifier.fillMaxSize(),
+    contentAlignment = Alignment.Center,
+    content = { IndeterminateProgressBar() }
+)
+
+@Composable
+private fun SourceScreenContentFailure(message: String) = Column(
+    modifier = Modifier.fillMaxSize(),
+    content = { SecondaryText(text = message, color = BooruchanTheme.colors.dimmed) }
+)
+
+@Composable
+private fun SourceScreenContentSuccess(
     screenState: SourceScreenState,
     contentState: ContentState.Success,
 ) {
-    SourceLazyVerticalStaggeredGrid(
-        screenState = screenState,
-        contentState = contentState,
-    )
+    val previewPostItems = contentState.pagerFlow.collectAsLazyPagingItems()
+    val refreshState = previewPostItems.loadState.refresh
+    val prependState = previewPostItems.loadState.prepend
+
+    when {
+        refreshState is LoadState.Loading && !prependState.endOfPaginationReached -> {
+            SourceScreenContentLoading()
+        }
+        refreshState is LoadState.Error -> {
+            SourceScreenContentFailure(message = refreshState.error.toString())
+        }
+        else -> {
+            SourceLazyVerticalStaggeredGrid(
+                screenState = screenState,
+                contentState = contentState,
+            )
+        }
+    }
 }
 
